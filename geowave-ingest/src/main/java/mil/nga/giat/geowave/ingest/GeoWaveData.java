e672ba2fedf97a8d44bc5425e28bff32571dc9ca
Rich Fecher <rfecher@gmail.com>
2015-01-06 10:12:52 -0800

    extension point for retyping vector data (issue #169)

--- geowave-ingest/src/main/java/mil/nga/giat/geowave/ingest/GeoWaveData.java
+++ geowave-ingest/src/main/java/mil/nga/giat/geowave/ingest/GeoWaveData.java
@@ -2,7 +2,6 @@ package mil.nga.giat.geowave.ingest;
 
 import mil.nga.giat.geowave.accumulo.mapreduce.output.GeoWaveOutputKey;
 import mil.nga.giat.geowave.index.ByteArrayId;
-import mil.nga.giat.geowave.ingest.local.IngestRunData;
 import mil.nga.giat.geowave.store.adapter.AdapterStore;
 import mil.nga.giat.geowave.store.adapter.DataAdapter;
 import mil.nga.giat.geowave.store.adapter.WritableDataAdapter;
@@ -12,7 +11,7 @@ import org.apache.log4j.Logger;
 /**
  * This models any information that is necessary to ingest an entry into
  * GeoWave: the adapter and index you wish to use as well as the actual data
- * 
+ *
  * @param <T>
  *            The java type for the actual data being ingested
  */
