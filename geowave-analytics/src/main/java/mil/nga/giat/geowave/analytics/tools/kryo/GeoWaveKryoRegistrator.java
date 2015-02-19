82e33096716f27362a7711866c0ac09b1fdc774a
rwgdrummer <rwgdrummer@gmail.com>
2015-02-19 20:15:22 +0000

    wip

--- geowave-analytics/src/main/java/mil/nga/giat/geowave/analytics/tools/kryo/GeoWaveKryoRegistrator.java
+++ geowave-analytics/src/main/java/mil/nga/giat/geowave/analytics/tools/kryo/GeoWaveKryoRegistrator.java
@@ -1,27 +1,31 @@
 package mil.nga.giat.geowave.analytics.tools.kryo;
 
-import org.apache.spark.SparkConf;
 import org.geotools.feature.simple.SimpleFeatureImpl;
 
 import com.esotericsoftware.kryo.Kryo;
 
+/**
+ * 
+ * @formatter:off
+ * 
+ *                SparkConf sparkConf;
+ * 
+ *                sparkConf.set( "spark.serializer",
+ *                "org.apache.spark.serializer.KryoSerializer");
+ * 
+ *                sparkConf.set( "spark.kryo.registrator",
+ *                GeoWaveKryoRegistrator.class.getName());
+ * 
+ * @formatter:on
+ */
 public class GeoWaveKryoRegistrator
 {
-	public static final void setupSerializers(
-			SparkConf sparkConf ) {
-		sparkConf.set(
-				"spark.serializer",
-				"org.apache.spark.serializer.KryoSerializer");
-		sparkConf.set(
-				"spark.kryo.registrator",
-				GeoWaveKryoRegistrator.class.getName());
-	}
 
 	public void registerClasses(
-			Kryo kryo ) {
+			final Kryo kryo ) {
 		kryo.register(
 				SimpleFeatureImpl.class,
 				new FeatureSerializer());
 	}
-	
+
 }
--- geowave-test/src/test/java/mil/nga/giat/geowave/test/GeoWaveITSuite.java
+++ geowave-test/src/test/java/mil/nga/giat/geowave/test/GeoWaveITSuite.java
@@ -1,6 +1,10 @@
 package mil.nga.giat.geowave.test;
 
-import mil.nga.giat.geowave.test.mapreduce.GeoWavePDBSCANIT;
+import mil.nga.giat.geowave.test.mapreduce.BasicMapReduceIT;
+import mil.nga.giat.geowave.test.mapreduce.GeoWaveKMeansIT;
+import mil.nga.giat.geowave.test.service.GeoServerIT;
+import mil.nga.giat.geowave.test.service.GeoWaveServicesIT;
+import mil.nga.giat.geowave.test.service.ServicesTestEnvironment;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
@@ -10,15 +14,14 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-//	GeoWaveBasicIT.class,
-//	GeoWaveRasterIT.class,
-//	BasicMapReduceIT.class,
-	GeoWavePDBSCANIT.class,
-//	GeoWaveKMeansIT.class,
+	GeoWaveBasicIT.class,
+	GeoWaveRasterIT.class,
+	BasicMapReduceIT.class,
+	GeoWaveKMeansIT.class,
 	// KDEMapReduceIT.class, //for now this is commented out, further
 	// investigation is required
-	//GeoServerIT.class,
-//	GeoWaveServicesIT.class
+	GeoServerIT.class,
+	GeoWaveServicesIT.class
 })
 public class GeoWaveITSuite
 {
@@ -33,7 +36,7 @@ public class GeoWaveITSuite
 	public static void cleanup() {
 		synchronized (GeoWaveTestEnvironment.MUTEX) {
 			GeoWaveTestEnvironment.DEFER_CLEANUP = false;
-		//	ServicesTestEnvironment.stopServices();
+			ServicesTestEnvironment.stopServices();
 			GeoWaveTestEnvironment.cleanup();
 		}
 	}
