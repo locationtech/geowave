2826ce5443adde0f3476b0ffbccf124047e727af
Chris Bennight <chris@slowcar.net>
2015-03-23 19:09:05 -0400

    added in changelog auto generation

---
+++ .utility/changelog-generator.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && [ "$TRAVIS_JDK_VERSION" == "oraclejdk7" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master"]
+then
+  gem install github_changelog_generator
+  github_changelog_generator
+  cp --parents CHANGELOG.md $HOME/site/CHANGELOG.md
+  echo -e "Published CHANGELOG.md to $HOME/site/CHANGELOG.md.\n"
+fi
