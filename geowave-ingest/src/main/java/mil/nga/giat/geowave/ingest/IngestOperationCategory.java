0a0bf15188c4505e16f967704155b69fe1a1f9fe
Rich Fecher <rfecher@gmail.com>
2015-04-15 07:12:33 -0400

    enabling pluggable CLI operations (issue #340)

---
+++ geowave-ingest/src/main/java/mil/nga/giat/geowave/ingest/IngestOperationCategory.java
@@ -0,0 +1,14 @@
+package mil.nga.giat.geowave.ingest;
+
+public class IngestOperationCategory extends
+		CustomOperationCategory
+{
+
+	public IngestOperationCategory() {
+		super(
+				"ingest",
+				"Ingest",
+				"Operations to read from common formats and write data to GeoWave");
+	}
+
+}
