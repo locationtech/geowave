b6c7007ec8e57d90289cd690cf6ac7144953d801
chrisbennight <chris@bennight.com>
2015-04-28 21:01:10 -0400

    removed mil.nga.giat.geowave.analytics.mapreduce.kde.parser.*

--- geowave-analytics/src/main/java/mil/nga/giat/geowave/analytics/mapreduce/kde/parser/CustomFunction.java
+++
@@ -1,9 +0,0 @@
-package mil.nga.giat.geowave.analytics.mapreduce.kde.parser;
-
-public interface CustomFunction
-{
-	public Object evaluate(
-			Object[] parameters );
-
-	public String getName();
-}
