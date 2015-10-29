fceb5a81b50c8bdd8dc2e0570a2510b0c2064c61
rwgdrummer <rwgdrummer@gmail.com>
2015-10-29 09:43:15 -0400

    Issue #528  Add unit test for a CQL temporal range query

---
+++ extensions/adapters/vector/src/test/java/mil/nga/giat/geowave/adapter/vector/plugin/WFSTemporalQueryTest.java
@@ -0,0 +1,165 @@
+package mil.nga.giat.geowave.adapter.vector.plugin;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.UUID;
+
+import mil.nga.giat.geowave.adapter.vector.utils.DateUtilities;
+import mil.nga.giat.geowave.core.geotime.IndexType;
+
+import org.apache.accumulo.core.client.AccumuloException;
+import org.apache.accumulo.core.client.AccumuloSecurityException;
+import org.geotools.data.DataUtilities;
+import org.geotools.data.DefaultTransaction;
+import org.geotools.data.FeatureReader;
+import org.geotools.data.FeatureWriter;
+import org.geotools.data.Query;
+import org.geotools.data.Transaction;
+import org.geotools.feature.SchemaException;
+import org.geotools.filter.text.cql2.CQL;
+import org.geotools.filter.text.cql2.CQLException;
+import org.junit.Before;
+import org.junit.Test;
+import org.opengis.feature.simple.SimpleFeature;
+import org.opengis.feature.simple.SimpleFeatureType;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.PrecisionModel;
+
+public class WFSTemporalQueryTest
+{
+	GeoWaveGTMemDataStore dataStore;
+	SimpleFeatureType schema;
+	SimpleFeatureType type;
+	final GeometryFactory factory = new GeometryFactory(
+			new PrecisionModel(
+					PrecisionModel.FIXED));
+
+	@Before
+	public void setup()
+			throws AccumuloException,
+			AccumuloSecurityException,
+			SchemaException,
+			CQLException,
+			IOException {
+		dataStore = new GeoWaveGTMemDataStore();
+		dataStore.dataStore.getIndexStore().addIndex(IndexType.SPATIAL_TEMPORAL_VECTOR.createDefaultIndex());
+		type = DataUtilities.createType(
+				"geostuff",
+				"geometry:Geometry:srid=4326,pop:java.lang.Long,pid:String,start:Date,end:Date");
+
+		dataStore.createSchema(type);
+
+	}
+
+	public void populate()
+			throws IOException,
+			CQLException,
+			ParseException {
+		final Transaction transaction1 = new DefaultTransaction();
+
+		final FeatureWriter<SimpleFeatureType, SimpleFeature> writer = dataStore.getFeatureWriter(
+				type.getTypeName(),
+				transaction1);
+		assertFalse(writer.hasNext());
+		SimpleFeature newFeature = writer.next();
+		newFeature.setAttribute(
+				"pop",
+				Long.valueOf(100));
+		newFeature.setAttribute(
+				"pid",
+				UUID.randomUUID().toString());
+		newFeature.setAttribute(
+				"start",
+				DateUtilities.parseISO("2005-05-17T20:32:56Z"));
+		newFeature.setAttribute(
+				"end",
+				DateUtilities.parseISO("2005-05-19T20:32:56Z"));
+		newFeature.setAttribute(
+				"geometry",
+				factory.createPoint(new Coordinate(
+						43.454,
+						28.232)));
+		writer.write();
+
+		newFeature = writer.next();
+		newFeature.setAttribute(
+				"pop",
+				Long.valueOf(100));
+		newFeature.setAttribute(
+				"pid",
+				UUID.randomUUID().toString());
+		newFeature.setAttribute(
+				"start",
+				DateUtilities.parseISO("2005-05-18T20:32:56Z"));
+		newFeature.setAttribute(
+				"end",
+				DateUtilities.parseISO("2005-05-20T20:32:56Z"));
+		newFeature.setAttribute(
+				"geometry",
+				factory.createPoint(new Coordinate(
+						43.454,
+						27.232)));
+		writer.write();
+
+		newFeature = writer.next();
+		newFeature.setAttribute(
+				"pop",
+				Long.valueOf(100));
+		newFeature.setAttribute(
+				"pid",
+				UUID.randomUUID().toString());
+		newFeature.setAttribute(
+				"start",
+				DateUtilities.parseISO("2005-05-21T20:32:56Z"));
+		newFeature.setAttribute(
+				"end",
+				DateUtilities.parseISO("2005-05-22T20:32:56Z"));
+		newFeature.setAttribute(
+				"geometry",
+				factory.createPoint(new Coordinate(
+						43.454,
+						28.232)));
+		writer.write();
+		writer.close();
+		transaction1.commit();
+		transaction1.close();
+	}
+
+	@Test
+	public void testTemporal()
+			throws CQLException,
+			IOException,
+			ParseException {
+
+		populate();
+		Transaction transaction2 = new DefaultTransaction();
+		Query query = new Query(
+				"geostuff",
+				CQL.toFilter("BBOX(geometry,44,27,42,30) and start during 2005-05-16T20:32:56Z/2005-05-20T21:32:56Z and end during 2005-05-18T20:32:56Z/2005-05-22T21:32:56Z"),
+				new String[] {
+					"geometry",
+					"pid"
+				});
+		FeatureReader<SimpleFeatureType, SimpleFeature> reader = dataStore.getFeatureReader(
+				query,
+				transaction2);
+		int c = 0;
+		while (reader.hasNext()) {
+			reader.next();
+			c++;
+		}
+		reader.close();
+		transaction2.commit();
+		transaction2.close();
+		assertEquals(
+				2,
+				c);
+
+	}
+
+}
