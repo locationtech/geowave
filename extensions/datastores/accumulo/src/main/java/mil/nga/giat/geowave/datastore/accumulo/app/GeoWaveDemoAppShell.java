7ce74c90b7d5b6ae05444fa85b67403a63a37204
Rich Fecher <rfecher@gmail.com>
2016-03-17 12:59:51 -0400

    GEOWAVE-669: moved geowavedemoapp to exmaples so that it could use adapter dependencies; demo app works in windows now

--- extensions/datastores/accumulo/src/main/java/mil/nga/giat/geowave/datastore/accumulo/app/GeoWaveDemoAppShell.java
+++ extensions/datastores/accumulo/src/main/java/mil/nga/giat/geowave/datastore/accumulo/app/GeoWaveDemoAppShell.java
@@ -1,5 +1,7 @@
 package mil.nga.giat.geowave.datastore.accumulo.app;
 
+import java.io.IOException;
+
 // @formatter:off
 /*if[accumulo.api=1.6]
 import org.apache.accumulo.core.util.shell.Shell;
@@ -10,21 +12,27 @@ import org.apache.accumulo.shell.Shell;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
-import java.io.IOException;
-
 public class GeoWaveDemoAppShell
 {
 
 	public static void main(
-			String[] args )
+			final String[] args )
 			throws IOException {
 		Logger.getRootLogger().setLevel(
 				Level.WARN);
 
-		final String instanceName = (System.getProperty("instanceName") != null) ? System.getProperty("instanceName") : "geowave";
-		final String password = (System.getProperty("password") != null) ? System.getProperty("password") : "password";
+		final String instanceName = (System.getProperty(
+				"instanceName") != null)
+						? System.getProperty(
+								"instanceName")
+						: "geowave";
+		final String password = (System.getProperty(
+				"password") != null)
+						? System.getProperty(
+								"password")
+						: "password";
 
-		String[] shellArgs = new String[] {
+		final String[] shellArgs = new String[] {
 			"-u",
 			"root",
 			"-p",
@@ -33,6 +41,8 @@ public class GeoWaveDemoAppShell
 			instanceName,
 			"localhost:2181"
 		};
-		Shell.main(shellArgs);
+
+		Shell.main(
+				shellArgs);
 	}
 }
