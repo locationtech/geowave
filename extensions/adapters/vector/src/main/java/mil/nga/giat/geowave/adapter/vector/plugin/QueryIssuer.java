ee7a97ea9d3c0736df18ed560725b1dfc0efef44
Rich Fecher <rfecher@gmail.com>
2016-06-27 15:49:14 -0400

    fixed an issue where cql filter optimization wasn't being used

--- extensions/adapters/vector/src/main/java/mil/nga/giat/geowave/adapter/vector/plugin/QueryIssuer.java
+++ extensions/adapters/vector/src/main/java/mil/nga/giat/geowave/adapter/vector/plugin/QueryIssuer.java
@@ -1,17 +1,17 @@
 package mil.nga.giat.geowave.adapter.vector.plugin;
 
-import mil.nga.giat.geowave.core.store.CloseableIterator;
-import mil.nga.giat.geowave.core.store.index.PrimaryIndex;
-import mil.nga.giat.geowave.core.store.query.Query;
-
 import org.opengis.feature.simple.SimpleFeature;
 import org.opengis.filter.Filter;
+
+import mil.nga.giat.geowave.core.store.CloseableIterator;
+import mil.nga.giat.geowave.core.store.index.PrimaryIndex;
+import mil.nga.giat.geowave.core.store.query.BasicQuery;
 
 public interface QueryIssuer
 {
 	CloseableIterator<SimpleFeature> query(
 			PrimaryIndex index,
-			Query constraints );
+			BasicQuery constraints );
 
 	Filter getFilter();
 
