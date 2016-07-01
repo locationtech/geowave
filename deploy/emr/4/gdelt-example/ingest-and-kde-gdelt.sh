44ef865062529b2907f7ffaf63b51590ab0f6835
mwhitby <mwhitby@radiantblue.com>
2016-07-01 11:16:33 -0400

    added the --config-file option

--- deploy/emr/4/gdelt-example/ingest-and-kde-gdelt.sh
+++ deploy/emr/4/gdelt-example/ingest-and-kde-gdelt.sh
@@ -13,7 +13,7 @@ accumulo shell -u root -p secret -e "grant NameSpace.CREATE_TABLE -ns geowave -u
 accumulo shell -u root -p secret -e "config -s general.vfs.context.classpath.geowave=hdfs://${HOSTNAME}:8020/accumulo/classpath/geowave/${GEOWAVE_VERSION}-apache/[^.].*.jar"
 accumulo shell -u root -p secret -e "config -ns geowave -s table.classpath.context=geowave"
 
-# Grab whatever gdelt data matches $TIME_REGEX. The example is set to 201603
+# Grab whatever gdelt data matches $TIME_REGEX. The example is set to 201602
 sudo mkdir $STAGING_DIR/gdelt;cd $STAGING_DIR/gdelt
 sudo wget http://data.gdeltproject.org/events/md5sums
 for file in `cat md5sums | cut -d' ' -f3 | grep "^${TIME_REGEX}"` ; do sudo wget http://data.gdeltproject.org/events/$file ; done
@@ -28,4 +28,4 @@ geowave ingest localtogw $STAGING_DIR/gdelt gdelt-accumulo gdelt-spatial -f gdel
 geowave config addstore -t accumulo gdelt-accumulo-out --gwNamespace geowave.kde_gdelt --zookeeper $HOSTNAME:2181 --instance $INSTANCE --user geowave --password geowave
 
 # Run a kde to produce a heatmap
-hadoop jar ${GEOWAVE_TOOLS_HOME}/geowave-tools.jar analytic kde --featureType gdeltevent --minLevel 5 --maxLevel 26 --minSplits $NUM_PARTITIONS --maxSplits $NUM_PARTITIONS --coverageName gdeltevent_kde --hdfsHostPort ${HOSTNAME}:${HDFS_PORT} --jobSubmissionHostPort ${HOSTNAME}:${RESOURCE_MAN_PORT} --tileSize 1 gdelt-accumulo gdelt-accumulo-out
+hadoop jar ${GEOWAVE_TOOLS_HOME}/geowave-tools.jar --config-file /home/hadoop/.geowave/0.9.2-config.properties analytic kde --featureType gdeltevent --minLevel 5 --maxLevel 26 --minSplits $NUM_PARTITIONS --maxSplits $NUM_PARTITIONS --coverageName gdeltevent_kde --hdfsHostPort ${HOSTNAME}:${HDFS_PORT} --jobSubmissionHostPort ${HOSTNAME}:${RESOURCE_MAN_PORT} --tileSize 1 gdelt-accumulo gdelt-accumulo-out
