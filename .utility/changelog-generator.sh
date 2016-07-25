bf57393a13e8830f05440f95fad41e6afa8738b0
rfecher <rfecher@gmail.com>
2016-07-25 09:47:08 -0400

    Update changelog-generator.sh

--- .utility/changelog-generator.sh
+++ .utility/changelog-generator.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]
+if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "0.9.2.1_branch" ]
 then
   gem install github_changelog_generator
   github_changelog_generator
