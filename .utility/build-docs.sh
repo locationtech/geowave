039faf7b55a585ce7a06bf57b322ad88d36de57b
Kent Miller <kmiller@radiantblue.com>
2016-10-18 13:21:33 -0700

    Set up travis to run unit- or integration-tests only

--- .utility/build-docs.sh
+++ .utility/build-docs.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+set -ev
 
-if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && $BUILD_DOCS && [ "$TRAVIS_BRANCH" == "master" ]; then
+if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && [ "$BUILD_DOCS" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo -e "Running mvn clean install...\n"
+  mvn -q clean install -Dfindbugs.skip -Daccumulo.version=$ACCUMULO_VERSION -Daccumulo.api=$ACCUMULO_API -Dhbase.version=$HBASE_VERSION -Dhadoop.version=$HADOOP_VERSION -Dgeotools.version=$GEOTOOLS_VERSION -Dgeoserver.version=$GEOSERVER_VERSION -DskipITs=true -DskipTests=true -Dformatter.skip=true -P $PLATFORM_VERSION
+  
   echo -e "Building docs...\n"
   mvn -P docs -pl docs install
   
