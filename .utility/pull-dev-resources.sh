c2171682f1cf6f9fa667214affd4d40b880d8c01
Kent Miller <kmiller@radiantblue.com>
2016-10-24 14:25:26 -0700

    Up to date w/ travis-trusty branch

--- .utility/pull-dev-resources.sh
+++ .utility/pull-dev-resources.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ev
+version=${DEV_RESOURCES_VERSION:=1.0}
+echo -e "Pull or build dev-resources-${version}.jar..."
 
 if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ]; then
-	echo -e "Verifying dev-resources jar...\n"
-	
 	# Pull dev-resources jar from S3
 	pushd dev-resources
 	if [ ! -d target ]; then
@@ -12,16 +12,19 @@ if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ]; then
 		echo -e "target folder exists"
 	fi
 	pushd target
-	if [ ! -f geowave-dev-resources-1.1.jar ]; then
+	if [ ! -f geowave-dev-resources-${version}.jar ]; then
 		echo -e "Pulling dev-resources jar from S3...\n"
-		wget https://s3.amazonaws.com/geowave.deploy.emr.4/geowave-clone/geowave-dev-resources-1.1.jar
+		wget https://s3.amazonaws.com/geowave.deploy.emr.4/geowave-clone/geowave-dev-resources-${version}.jar
 	else
 		echo -e "dev-resources jar exists"
 	fi
+	popd
+	if [ -f target/geowave-dev-resources-${version}.jar ]; then
+		# Install downloaded jar to local repo
+		mvn install:install-file -Dfile=target/geowave-dev-resources-${version}.jar -DgroupId=mil.nga.giat -DartifactId=geowave-dev-resources -Dversion=${version} -Dpackaging=jar
+	else
 	# Fallback to building the jar if the S3 pull failed
-	if [ ! -f geowave-dev-resources-1.1.jar ]; then
 		echo -e "Pull from S3 failed. Building..."
-		popd
 		mvn clean install
 	fi
 fi
