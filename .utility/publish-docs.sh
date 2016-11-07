8912404e259f1e0f501bbf9b8c0a480eb1505fd1
mwhitby <mwhitby@radiantblue.com>
2016-11-07 15:07:48 -0500

    setting up documentation versioning

--- .utility/publish-docs.sh
+++ .utility/publish-docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "mawhitby/geowave" ] && [ "$BUILD_DOCS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then  
+if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && [ "$BUILD_DOCS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then  
   echo -e "Generating changelog...\n"
   gem install github_changelog_generator
   github_changelog_generator
@@ -15,7 +15,6 @@ if [ "$TRAVIS_REPO_SLUG" == "mawhitby/geowave" ] && [ "$BUILD_DOCS" == "true" ] 
   cp -R target/site $HOME/site
 
   cd $HOME
-  echo $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ngageoint/geowave gh-pages > /dev/null
