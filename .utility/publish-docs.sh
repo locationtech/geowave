13da300fd8f14715790e6c0065d576c728026348
mwhitby <mwhitby@radiantblue.com>
2016-11-07 15:04:27 -0500

    setting up documentation versioning

--- .utility/publish-docs.sh
+++ .utility/publish-docs.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 
-if [ "$TRAVIS_REPO_SLUG" == "ngageoint/geowave" ] && [ "$BUILD_DOCS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then  
+if [ "$TRAVIS_REPO_SLUG" == "mawhitby/geowave" ] && [ "$BUILD_DOCS" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then  
   echo -e "Generating changelog...\n"
   gem install github_changelog_generator
   github_changelog_generator
   pandoc -f markdown -t html -s -c stylesheets/changelog.css CHANGELOG.md > changelog.html
   cp changelog.html target/site/
   
+  # Get the version from the build.properties file
+  filePath=deploy/target/classes/build.properties
+  GEOWAVE_VERSION=$(grep project.version $filePath|  awk -F= '{print $2}')
+
   echo -e "Publishing site ...\n"
   cp -R target/site $HOME/site
 
   cd $HOME
+  echo $HOME
   git config --global user.email "travis@travis-ci.org"
   git config --global user.name "travis-ci"
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/ngageoint/geowave gh-pages > /dev/null
 
-  cd gh-pages
+  cd gh-pages 
+
+  # Check if this is not a snapshot version
+  if [[ ! "$GEOWAVE_VERSION" =~ "SNAPSHOT" ]] ; then
+
+    # Make a new directory for this release and copy the contents to it
+    echo "creating a new versioned documentation directory"
+    mkdir previous-versions/$GEOWAVE_VERSION
+    cp -Rf $HOME/site/* previous-versions/$GEOWAVE_VERSION/
+    rm -f previous-versions/$GEOWAVE_VERSION/*.epub *.pdf *.pdfmarks
+
+  fi
+
+  # Save previous versions of the documentation
+  cp -r previous-versions/ $HOME/site/
+
   git rm -rf .
   cp -Rf $HOME/site/* .
 
