41ffe6c5c6b5a98a23354da1da2854f9278d6eb6
Rich Fecher <rfecher@gmail.com>
2016-12-30 17:09:54 -0500

    wip: experiment with the cassandra IT

--- extensions/datastores/accumulo/src/main/java/mil/nga/giat/geowave/datastore/accumulo/minicluster/MiniAccumuloClusterFactory.java
+++ extensions/datastores/accumulo/src/main/java/mil/nga/giat/geowave/datastore/accumulo/minicluster/MiniAccumuloClusterFactory.java
@@ -1,33 +1,24 @@
 package mil.nga.giat.geowave.datastore.accumulo.minicluster;
 
 import java.io.File;
-import java.io.FileFilter;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.net.URLClassLoader;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Map;
-import java.util.jar.Attributes;
-import java.util.jar.JarOutputStream;
-import java.util.jar.Manifest;
 
 import org.apache.accumulo.minicluster.impl.MiniAccumuloClusterImpl;
 import org.apache.accumulo.minicluster.impl.MiniAccumuloConfigImpl;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.SystemUtils;
-import org.apache.commons.vfs2.FileObject;
-import org.apache.commons.vfs2.impl.VFSClassLoader;
 import org.apache.hadoop.util.VersionInfo;
 import org.apache.hadoop.util.VersionUtil;
 import org.apache.log4j.Logger;
 
+import mil.nga.giat.geowave.core.store.util.ClasspathUtils;
+
 public class MiniAccumuloClusterFactory
 {
 
-	private static final Logger LOGGER = Logger.getLogger(MiniAccumuloClusterFactory.class);
+	private static final Logger LOGGER = Logger.getLogger(
+			MiniAccumuloClusterFactory.class);
 
 	protected static final String HADOOP_WINDOWS_UTIL = "winutils.exe";
 
@@ -42,7 +33,7 @@ public class MiniAccumuloClusterFactory
 			final Class context )
 			throws IOException {
 
-		final String jarPath = setupPathingJarClassPath(
+		final String jarPath = ClasspathUtils.setupPathingJarClassPath(
 				config.getDir(),
 				context);
 
@@ -51,9 +42,10 @@ public class MiniAccumuloClusterFactory
 			return null;
 		}
 
-		config.setClasspathItems(jarPath);
+		config.setClasspathItems(
+				jarPath);
 
-		MiniAccumuloClusterImpl retVal = new GeoWaveMiniAccumuloClusterImpl(
+		final MiniAccumuloClusterImpl retVal = new GeoWaveMiniAccumuloClusterImpl(
 				config);
 		if (SystemUtils.IS_OS_WINDOWS && isYarn()) {
 			// this must happen after instantiating Mini
@@ -64,16 +56,20 @@ public class MiniAccumuloClusterFactory
 			// directory in the mini accumulo cluster directory
 			// (mini accumulo cluster will always set this
 			// directory as hadoop_home)
-			LOGGER.info("Running YARN on windows requires a local installation of Hadoop");
-			LOGGER.info("'HADOOP_HOME' must be set and 'PATH' must contain %HADOOP_HOME%/bin");
+			LOGGER.info(
+					"Running YARN on windows requires a local installation of Hadoop");
+			LOGGER.info(
+					"'HADOOP_HOME' must be set and 'PATH' must contain %HADOOP_HOME%/bin");
 
 			final Map<String, String> env = System.getenv();
 			// HP Fortify "Path Manipulation" false positive
 			// What Fortify considers "user input" comes only
 			// from users with OS-level access anyway
-			String hadoopHome = System.getProperty("hadoop.home.dir");
+			String hadoopHome = System.getProperty(
+					"hadoop.home.dir");
 			if (hadoopHome == null) {
-				hadoopHome = env.get("HADOOP_HOME");
+				hadoopHome = env.get(
+						"HADOOP_HOME");
 			}
 			boolean success = false;
 			if (hadoopHome != null) {
@@ -96,162 +92,13 @@ public class MiniAccumuloClusterFactory
 				}
 			}
 			if (!success) {
-				LOGGER
-						.error("'HADOOP_HOME' environment variable is not set or <HADOOP_HOME>/bin/winutils.exe does not exist");
+				LOGGER.error(
+						"'HADOOP_HOME' environment variable is not set or <HADOOP_HOME>/bin/winutils.exe does not exist");
 
 				// return mini accumulo cluster anyways
 				return retVal;
 			}
 		}
 		return retVal;
-	}
-
-	private static String setupPathingJarClassPath(
-			final File dir,
-			final Class context )
-			throws IOException {
-
-		final String classpath = getClasspath(context);
-
-		final File jarDir = new File(
-				dir.getParentFile().getAbsolutePath() + File.separator + "pathing");
-		if (!jarDir.exists()) {
-			try {
-				jarDir.mkdirs();
-			}
-			catch (final Exception e) {
-				LOGGER.error("Failed to create pathing jar directory: " + e);
-				return null;
-			}
-		}
-
-		final File jarFile = new File(
-				jarDir,
-				"pathing.jar");
-
-		if (jarFile.exists()) {
-			try {
-				jarFile.delete();
-			}
-			catch (final Exception e) {
-				LOGGER.error("Failed to delete old pathing jar: " + e);
-				return null;
-			}
-		}
-
-		// build jar
-		final Manifest manifest = new Manifest();
-		manifest.getMainAttributes().put(
-				Attributes.Name.MANIFEST_VERSION,
-				"1.0");
-		manifest.getMainAttributes().put(
-				Attributes.Name.CLASS_PATH,
-				classpath);
-
-		try (final JarOutputStream target = new JarOutputStream(
-				new FileOutputStream(
-						jarFile),
-				manifest)) {
-
-			target.close();
-		}
-
-		return jarFile.getAbsolutePath();
-	}
-
-	private static String getClasspath(
-			final Class context )
-			throws IOException {
-
-		try {
-			final ArrayList<ClassLoader> classloaders = new ArrayList<ClassLoader>();
-
-			ClassLoader cl = context.getClassLoader();
-
-			while (cl != null) {
-				classloaders.add(cl);
-				cl = cl.getParent();
-			}
-
-			Collections.reverse(classloaders);
-
-			final StringBuilder classpathBuilder = new StringBuilder();
-
-			// assume 0 is the system classloader and skip it
-			for (int i = 0; i < classloaders.size(); i++) {
-				final ClassLoader classLoader = classloaders.get(i);
-
-				if (classLoader instanceof URLClassLoader) {
-
-					for (final URL u : ((URLClassLoader) classLoader).getURLs()) {
-						append(
-								classpathBuilder,
-								u);
-					}
-
-				}
-				else if (classLoader instanceof VFSClassLoader) {
-
-					final VFSClassLoader vcl = (VFSClassLoader) classLoader;
-					for (final FileObject f : vcl.getFileObjects()) {
-						append(
-								classpathBuilder,
-								f.getURL());
-					}
-				}
-				else {
-					throw new IllegalArgumentException(
-							"Unknown classloader type : " + classLoader.getClass().getName());
-				}
-			}
-
-			classpathBuilder.deleteCharAt(0);
-			return classpathBuilder.toString();
-
-		}
-		catch (final URISyntaxException e) {
-			throw new IOException(
-					e);
-		}
-	}
-
-	private static boolean containsSiteFile(
-			final File f ) {
-		if (f.isDirectory()) {
-			final File[] sitefile = f.listFiles(new FileFilter() {
-				@Override
-				public boolean accept(
-						final File pathname ) {
-					return pathname.getName().endsWith(
-							"site.xml");
-				}
-			});
-
-			return (sitefile != null) && (sitefile.length > 0);
-		}
-		return false;
-	}
-
-	private static void append(
-			final StringBuilder classpathBuilder,
-			final URL url )
-			throws URISyntaxException {
-
-		final File file = new File(
-				url.toURI());
-
-		// do not include dirs containing hadoop or accumulo site files
-		if (!containsSiteFile(file)) {
-			classpathBuilder.append(
-					" ").append(
-					file.getAbsolutePath().replace(
-							"C:\\",
-							"file:/C:/").replace(
-							"\\",
-							"/"));
-			if (file.isDirectory()) {
-				classpathBuilder.append("/");
-			}
-		}
 	}
 }
--- test/src/main/java/mil/nga/giat/geowave/test/CassandraStoreTestEnvironment.java
+++ test/src/main/java/mil/nga/giat/geowave/test/CassandraStoreTestEnvironment.java
@@ -1,23 +1,178 @@
 package mil.nga.giat.geowave.test;
 
-import org.apache.log4j.Logger;
+import java.io.File;
+import java.io.IOException;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
+import org.apache.maven.artifact.DefaultArtifact;
+import org.apache.maven.artifact.handler.DefaultArtifactHandler;
+import org.apache.maven.artifact.versioning.InvalidVersionSpecificationException;
+import org.apache.maven.artifact.versioning.VersionRange;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.mojo.cassandra.AbstractCassandraMojo;
+import org.codehaus.mojo.cassandra.StartCassandraClusterMojo;
+import org.codehaus.mojo.cassandra.StopCassandraClusterMojo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import mil.nga.giat.geowave.core.store.DataStore;
 import mil.nga.giat.geowave.core.store.GenericStoreFactory;
 import mil.nga.giat.geowave.core.store.StoreFactoryOptions;
+import mil.nga.giat.geowave.core.store.util.ClasspathUtils;
+import mil.nga.giat.geowave.datastore.cassandra.CassandraDataStoreFactory;
+import mil.nga.giat.geowave.datastore.cassandra.operations.config.CassandraRequiredOptions;
 import mil.nga.giat.geowave.test.annotation.GeoWaveTestStore.GeoWaveStoreType;
 
 public class CassandraStoreTestEnvironment extends
 		StoreTestEnvironment
 {
-	private static GenericStoreFactory<DataStore> STORE_FACTORY;
-	private static CassandraStoreTestEnvironment singletonInstance = null;
+	private final static Logger LOGGER = LoggerFactory.getLogger(
+			CassandraStoreTestEnvironment.class);
 
-	private Cluster cluster;
-	private Session session;
+	private static CassandraDataStoreFactory STORE_FACTORY;
+	private static CassandraStoreTestEnvironment singletonInstance = null;
+	protected static final File TEMP_DIR = new File(
+			"./target/cassandra_temp/");
+
+	private static class StartGeoWaveCluster extends
+			StartCassandraClusterMojo
+	{
+		public StartGeoWaveCluster() {
+			super();
+			startWaitSeconds = 180;
+			rpcAddress = "127.0.0.1";
+			rpcPort = 9160;
+			jmxPort = 7199;
+			startNativeTransport = true;
+			nativeTransportPort = 9042;
+			listenAddress = "127.0.0.1";
+			storagePort = 7000;
+			stopPort = 8081;
+			stopKey = "cassandra-maven-plugin";
+			maxMemory = 512;
+			cassandraDir = TEMP_DIR;
+			cassandraDir.mkdirs();
+			project = new MavenProject();
+			project.setFile(
+					cassandraDir);
+			Field f;
+			try {
+				f = StartCassandraClusterMojo.class.getDeclaredField(
+						"clusterSize");
+				f.setAccessible(
+						true);
+				f.set(
+						this,
+						4);
+				f = AbstractCassandraMojo.class.getDeclaredField(
+						"pluginArtifact");
+
+				f.setAccessible(
+						true);
+				final DefaultArtifact a = new DefaultArtifact(
+						"group",
+						"artifact",
+						VersionRange.createFromVersionSpec(
+								"version"),
+						null,
+						"type",
+						null,
+						new DefaultArtifactHandler());
+				a.setFile(
+						cassandraDir);
+				f.set(
+						this,
+						a);
+
+				f = AbstractCassandraMojo.class.getDeclaredField(
+						"pluginDependencies");
+				f.setAccessible(
+						true);
+				f.set(
+						this,
+						new ArrayList<>());
+			}
+			catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException
+					| InvalidVersionSpecificationException e) {
+				LOGGER.error(
+						"Unable to initialize start cassandra cluster",
+						e);
+			}
+		}
+
+		@Override
+		protected void createCassandraJar(
+				File jarFile,
+				String mainClass,
+				File cassandraDir )
+				throws IOException {
+			ClasspathUtils.setupPathingJarClassPath(jarFile, mainClass, this.getClass());
+//			super.createCassandraJar(
+//					jarFile,
+//					mainClass,
+//					cassandraDir);
+		}
+
+		public void start() {
+			try {
+				super.execute();
+			}
+			catch (MojoExecutionException | MojoFailureException e) {
+				LOGGER.error(
+						"Unable to start cassandra cluster",
+						e);
+			}
+		}
+
+	}
+
+	private static class StopGeoWaveCluster extends
+			StopCassandraClusterMojo
+	{
+		public StopGeoWaveCluster() {
+			super();
+			rpcPort = 9160;
+			stopPort = 8081;
+			stopKey = "cassandra-maven-plugin";
+			Field f;
+			try {
+				f = StopCassandraClusterMojo.class.getDeclaredField(
+						"clusterSize");
+				f.setAccessible(
+						true);
+				f.set(
+						this,
+						4);
+				f = StopCassandraClusterMojo.class.getDeclaredField(
+						"rpcAddress");
+
+				f.setAccessible(
+						true);
+				f.set(
+						this,
+						"127.0.0.1");
+			}
+			catch (NoSuchFieldException | SecurityException | IllegalArgumentException | IllegalAccessException e) {
+				LOGGER.error(
+						"Unable to initialize stop cassandra cluster",
+						e);
+			}
+		}
+
+		public void stop() {
+			try {
+				super.execute();
+			}
+			catch (MojoExecutionException | MojoFailureException e) {
+				LOGGER.error(
+						"Unable to stop cassandra cluster",
+						e);
+			}
+		}
+	}
 
 	public static synchronized CassandraStoreTestEnvironment getInstance() {
 		if (singletonInstance == null) {
@@ -26,13 +181,15 @@ public class CassandraStoreTestEnvironment extends
 		return singletonInstance;
 	}
 
-	private final static Logger LOGGER = Logger.getLogger(CassandraStoreTestEnvironment.class);
-
 	private CassandraStoreTestEnvironment() {}
 
 	@Override
 	protected void initOptions(
-			final StoreFactoryOptions options ) {}
+			final StoreFactoryOptions options ) {
+		final CassandraRequiredOptions cassandraOpts = (CassandraRequiredOptions) options;
+		cassandraOpts.setContactPoint(
+				"127.0.0.1");
+	}
 
 	@Override
 	protected GenericStoreFactory<DataStore> getDataStoreFactory() {
@@ -41,61 +198,12 @@ public class CassandraStoreTestEnvironment extends
 
 	@Override
 	public void setup() {
-		try {
-			if (cluster == null) {
-				cluster = Cluster.builder().addContactPoint(
-						"127.0.0.1").build();
-			}
-
-			session = cluster.connect();
-
-			STORE_FACTORY = new GenericStoreFactory<DataStore>() {
-
-				@Override
-				public String getType() {
-					// TODO Auto-generated method stub
-					return "cassandra";
-				}
-
-				@Override
-				public String getDescription() {
-					// TODO Auto-generated method stub
-					return "Cassandra test store";
-				}
-
-				@Override
-				public DataStore createStore(
-						StoreFactoryOptions options ) {
-					// TODO Auto-generated method stub
-					return null;
-				}
-
-				@Override
-				public StoreFactoryOptions createOptionsInstance() {
-					// TODO Auto-generated method stub
-					return null;
-				}
-			};
-
-			LOGGER.info("Opened connection to cassandra cluster!");
-		}
-		catch (Exception e) {
-			LOGGER.error(
-					"Failed to connect to Cassandra test cluster",
-					e);
-		}
+		new StartGeoWaveCluster().start();
 	}
 
 	@Override
 	public void tearDown() {
-		try {
-			session.close();
-			cluster.close();
-		}
-		catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		new StopGeoWaveCluster().stop();
 	}
 
 	@Override
