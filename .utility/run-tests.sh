ef2182d32b0aaa4b8b93260e5aea8118f9c5c0d8
rfecher <rfecher@gmail.com>
2017-01-21 12:11:50 -0500

    simplifying travis build matrix using profiles (#991)

--- .utility/run-tests.sh
+++ .utility/run-tests.sh
@@ -3,8 +3,8 @@ set -ev
 
 if [ "$IT_ONLY" == "true" ]; then
   echo -e "Skipping unit tests w/ verify...\n"
-  mvn -q verify -Dtest=SkipUnitTests -DfailIfNoTests=false -Daccumulo.version=$ACCUMULO_VERSION -Daccumulo.api=$ACCUMULO_API -Dhbase.version=$HBASE_VERSION -Dhadoop.version=$HADOOP_VERSION -Dgeotools.version=$GEOTOOLS_VERSION -Dgeoserver.version=$GEOSERVER_VERSION -P $PLATFORM_VERSION
+  mvn -q verify -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
 else
   echo -e "Running unit tests only w/ verify...\n"
-  mvn -q verify -DskipITs=true -Daccumulo.version=$ACCUMULO_VERSION -Daccumulo.api=$ACCUMULO_API -Dhbase.version=$HBASE_VERSION -Dhadoop.version=$HADOOP_VERSION -Dgeotools.version=$GEOTOOLS_VERSION -Dgeoserver.version=$GEOSERVER_VERSION -P $PLATFORM_VERSION
+  mvn -q verify -P $MAVEN_PROFILES
 fi
