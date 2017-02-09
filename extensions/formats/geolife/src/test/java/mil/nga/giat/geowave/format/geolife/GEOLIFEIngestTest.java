7fe58eb694014a13ae814a07398e37f844d71684
Rebecca <reb343@cornell.edu>
2017-02-09 00:06:59 -0500

    working of tests for geolife and avro - still need resources to test from

--- extensions/formats/geolife/src/test/java/mil/nga/giat/geowave/format/geolife/GEOLIFEIngestTest.java
+++ extensions/formats/geolife/src/test/java/mil/nga/giat/geowave/format/geolife/GEOLIFEIngestTest.java
@@ -1,4 +1,4 @@
-package mil.nga.giat.geowave.format.avro;
+package mil.nga.giat.geowave.format.geolife;
 
 import static org.junit.Assert.assertTrue;
 
@@ -7,24 +7,21 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
-import mil.nga.giat.geowave.adapter.vector.avro.AvroSimpleFeatureCollection;
 import mil.nga.giat.geowave.adapter.vector.ingest.DataSchemaOptionProvider;
 import mil.nga.giat.geowave.core.index.ByteArrayId;
 import mil.nga.giat.geowave.core.index.StringUtils;
 import mil.nga.giat.geowave.core.ingest.GeoWaveData;
 import mil.nga.giat.geowave.core.store.CloseableIterator;
 
-import org.apache.avro.file.DataFileReader;
-import org.apache.avro.specific.SpecificDatumReader;
 import org.junit.Before;
 import org.junit.Test;
 import org.opengis.feature.simple.SimpleFeature;
 
-public class AVROIngestTest
+public class GEOLIFEIngestTest
 {
 	private DataSchemaOptionProvider optionsProvider;
-	private AvroIngestPlugin ingester;
-	private AvroIngestPlugin ingesterExt;
+	private GeoLifeIngestPlugin ingester;
+	//private GDELTIngestPlugin ingesterExt;
 	private String filePath;
 	private int expectedCount;
 
@@ -33,11 +30,15 @@ public class AVROIngestTest
 		optionsProvider = new DataSchemaOptionProvider();
 		optionsProvider.setSupplementalFields(true);
 
-		ingester = new AvroIngestPlugin(); //TODO update constructor??
+		ingester = new GeoLifeIngestPlugin();
 		ingester.init(null);
 
-		filePath = "20130401.export.CSV.zip"; //TODO is this the right data to use from AVRO?
-		expectedCount = 14056;
+		/*ingesterExt = new GeoLifeIngestPlugin( //TODO delete this late if det. don't need
+				optionsProvider);
+		ingesterExt.init(null);*/
+
+		filePath = "20130401.export.CSV.zip"; // TODO this is probably not the right data to use
+		expectedCount = 14056; // TODO get the right expected count
 	}
 
 	@Test
@@ -48,7 +49,7 @@ public class AVROIngestTest
 				this.getClass().getClassLoader().getResource(
 						filePath).getPath());
 
-		assertTrue(validate(toIngest));
+		assertTrue(GeoLifeUtils.validate(toIngest));
 		final Collection<ByteArrayId> indexIds = new ArrayList<ByteArrayId>();
 		indexIds.add(new ByteArrayId(
 				"123".getBytes(StringUtils.UTF8_CHAR_SET)));
@@ -63,19 +64,28 @@ public class AVROIngestTest
 		while (features.hasNext()) {
 			final GeoWaveData<SimpleFeature> feature = features.next();
 
-			if (isValidGDELTFeature(feature)) { //TODO replace with isValidAVRO
+			if (isValidGeoLifeFeature(feature)) {
 				featureCount++;
 			}
 		}
 		features.close();
 
-		final CloseableIterator<GeoWaveData<SimpleFeature>> featuresExt = ingesterExt.toGeoWaveData(
-				toIngest,
-				indexIds,
-				"");
+//		final CloseableIterator<GeoWaveData<SimpleFeature>> featuresExt = ingesterExt.toGeoWaveData(
+//				toIngest,
+//				indexIds,
+//				"");
 
-		assertTrue((featuresExt != null) && featuresExt.hasNext());
-
+//		assertTrue((featuresExt != null) && featuresExt.hasNext());
+//
+//		int featureCountExt = 0;
+//		while (featuresExt.hasNext()) {
+//			final GeoWaveData<SimpleFeature> featureExt = featuresExt.next();
+//
+//			if (isValidGDELTFeatureExt(featureExt)) {
+//				featureCountExt++;
+//			}
+//		}
+//		featuresExt.close();
 
 		final boolean readExpectedCount = (featureCount == expectedCount);
 		if (!readExpectedCount) {
@@ -85,39 +95,39 @@ public class AVROIngestTest
 		assertTrue(readExpectedCount);
 	}
 	
-	/* Features avro should have:
-	 * adapter
-	 * primaryIndexIds
-	 * simpleFeature
+	/* PointBuilder things that should be set:
+	 * geometry
+	 * trackid
+	 * pointinstance
+	 * Timestamp
+	 * Latitude
+	 * Longitude
+	 */
+	/* PointBuilder things that can be null:
+	 * elevation
+	 */
+	/* TrackBuilder things that should be set:
+	 * geometry
+	 * StartTimeStamp?
+	 * EndTimeStamp?
+	 * NumberPoints
+	 * TrackId
+	 */
+	/* TrackBuilder things that can be null:
+	 * Duration
 	 */
 
-	private boolean isValidGDELTFeature( //TODO replace this function
+	private boolean isValidGeoLifeFeature(	
 			final GeoWaveData<SimpleFeature> feature ) {
 		if ((feature.getValue().getAttribute(
-				"adapter") == null) || (feature.getValue().getAttribute(
-				"primaryIndexIds") == null) || (feature.getValue().getAttribute(
-				"simpleFeature") == null)) {
+				"geometry") == null) || (feature.getValue().getAttribute(
+				"trackid") == null) || (feature.getValue().getAttribute(
+				"pointinstance") == null) || (feature.getValue().getAttribute(
+				"Latitue") == null) || (feature.getValue().getAttribute(
+				"Longitude") == null)) {
 			return false;
 		}
 		return true;
 	}
 
-	private boolean validate(File file){
-		try {
-			DataFileReader.openReader(
-					file,
-					new SpecificDatumReader<AvroSimpleFeatureCollection>()).close();
-			return true;
-		}
-		catch (final IOException e) {
-			// TODO log this??
-			// just log as info as this may not have been intended to be read as
-			// avro vector data
-//			LOGGER.info(
-//					"Unable to read file as Avro vector data '" + file.getName() + "'",
-//					e);
-		}
-
-		return false;
-	}
 }
