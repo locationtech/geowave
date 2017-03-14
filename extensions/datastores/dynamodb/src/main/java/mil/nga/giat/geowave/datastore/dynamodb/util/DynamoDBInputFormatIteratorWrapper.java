fb76a8989a7a6211356f7631d62bb9fd69e3cf6b
Akash Peri <akash.peri@gmail.com>
2017-03-14 15:49:39 -0400

    Update comment

--- extensions/datastores/dynamodb/src/main/java/mil/nga/giat/geowave/datastore/dynamodb/util/DynamoDBInputFormatIteratorWrapper.java
+++ extensions/datastores/dynamodb/src/main/java/mil/nga/giat/geowave/datastore/dynamodb/util/DynamoDBInputFormatIteratorWrapper.java
@@ -21,7 +21,7 @@ import mil.nga.giat.geowave.mapreduce.input.GeoWaveInputKey;
  * the appropriate data adapter). It also performs any client-side filtering. It
  * will peek at the next entry in the HBase iterator to always maintain a
  * reference to the next value. It maintains the adapter ID, data ID, and
- * original HBase key in the GeoWaveInputKey for use by the GeoWaveInputFormat.
+ * original DynamoDB key in the GeoWaveInputKey for use by the GeoWaveInputFormat.
  * 
  * @param <T>
  *            The type for the entry
