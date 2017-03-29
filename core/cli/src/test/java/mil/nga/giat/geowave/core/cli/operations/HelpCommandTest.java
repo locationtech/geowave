f416d46a6260cf8cf53d71a0cddc027064ca7255
Ashish <ashish217@users.noreply.github.com>
2017-03-29 08:43:47 -0400

    Added capability to retain geometric predicate associated with query geometry extracted from CQL expression. (#1055)

--- core/cli/src/test/java/mil/nga/giat/geowave/core/cli/operations/HelpCommandTest.java
+++ core/cli/src/test/java/mil/nga/giat/geowave/core/cli/operations/HelpCommandTest.java
@@ -11,40 +11,54 @@ import mil.nga.giat.geowave.core.cli.parser.CommandLineOperationParams;
 import mil.nga.giat.geowave.core.cli.parser.OperationParser;
 import mil.nga.giat.geowave.core.cli.spi.OperationRegistry;
 
-public class HelpCommandTest {
+public class HelpCommandTest
+{
 
 	@Test
 	public void testPrepare() {
-		String[] args = {"help"};
+		String[] args = {
+			"help"
+		};
 		OperationRegistry registry = OperationRegistry.getInstance();
-		OperationParser parser = new OperationParser(registry);
+		OperationParser parser = new OperationParser(
+				registry);
 		final CommandLineOperationParams params = parser.parse(
 				GeowaveTopLevelSection.class,
 				args);
-		
+
 		HelpCommand helpcommand = new HelpCommand();
 		helpcommand.prepare(params);
-		assertEquals(false, params.isValidate());
-		assertEquals(true, params.isAllowUnknown());
+		assertEquals(
+				false,
+				params.isValidate());
+		assertEquals(
+				true,
+				params.isAllowUnknown());
 	}
-	
+
 	@Test
 	public void testExecute() {
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(output));
-		
-		String[] args = {"help"};
+		System.setOut(new PrintStream(
+				output));
+
+		String[] args = {
+			"help"
+		};
 		OperationRegistry registry = OperationRegistry.getInstance();
-		OperationParser parser = new OperationParser(registry);
+		OperationParser parser = new OperationParser(
+				registry);
 		final CommandLineOperationParams params = parser.parse(
 				GeowaveTopLevelSection.class,
 				args);
-		
+
 		HelpCommand helpcommand = new HelpCommand();
 		helpcommand.execute(params);
-		
+
 		String expectedoutput = "Usage: geowave help [options]\n";
-		assertEquals(expectedoutput, output.toString());
+		assertEquals(
+				expectedoutput,
+				output.toString());
 	}
 
 }
