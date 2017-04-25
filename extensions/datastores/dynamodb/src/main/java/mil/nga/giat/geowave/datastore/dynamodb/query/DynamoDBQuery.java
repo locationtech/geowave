61a6cc90075aa3ed9400499faeb5105603780ecd
anicr7 <anicr7@gmail.com>
2017-04-25 08:49:54 -0400

    Geowave 1026[Async support for query and scan in DynamoDB] (#1072)
    * Adding support for asynchronous query in DynamoDBQuery
    
    * Make the Async Paginated Query cleaner with the following changes:-
    
    1. Use a intrinsic sync block instead of specific re-entrant locks using try and finally.
    2. Use linked list instead of array deque to allow inserting null locks. This prevents wait forevers when we have exception in async queries
    3. Make sure we don't fire queries immediately on construction if there are already too many async queries piled up.
    4. Reduce the max async query limit to 100. This can be made configurable through metadata in the future

--- extensions/datastores/dynamodb/src/main/java/mil/nga/giat/geowave/datastore/dynamodb/query/DynamoDBQuery.java
+++ extensions/datastores/dynamodb/src/main/java/mil/nga/giat/geowave/datastore/dynamodb/query/DynamoDBQuery.java
@@ -30,6 +30,7 @@ import mil.nga.giat.geowave.datastore.dynamodb.DynamoDBDataStore;
 import mil.nga.giat.geowave.datastore.dynamodb.DynamoDBOperations;
 import mil.nga.giat.geowave.datastore.dynamodb.DynamoDBRow;
 import mil.nga.giat.geowave.datastore.dynamodb.util.AsyncPaginatedQuery;
+import mil.nga.giat.geowave.datastore.dynamodb.util.AsyncPaginatedScan;
 import mil.nga.giat.geowave.datastore.dynamodb.util.LazyPaginatedQuery;
 import mil.nga.giat.geowave.datastore.dynamodb.util.LazyPaginatedScan;
 
@@ -146,15 +147,26 @@ abstract public class DynamoDBQuery extends
 								this::executeQueryRequest).iterator()); 
 			}
 		}
-		// query everything
-		final ScanRequest request = new ScanRequest(
-				tableName);
-		final ScanResult scanResult = dynamodbOperations.getClient().scan(
-				request);
-		return new LazyPaginatedScan(
-				scanResult,
-				request,
-				dynamodbOperations.getClient());
+		
+		if(async){
+			final ScanRequest request = new ScanRequest(
+					tableName);
+			return new AsyncPaginatedScan(
+					request,
+					dynamodbOperations.getClient());
+		}
+		else{
+			// query everything
+			final ScanRequest request = new ScanRequest(
+					tableName);
+			final ScanResult scanResult = dynamodbOperations.getClient().scan(
+					request);
+			return new LazyPaginatedScan(
+					scanResult,
+					request,
+					dynamodbOperations.getClient());
+		}
+
 	}
 
 	private List<QueryRequest> addQueryRanges(
