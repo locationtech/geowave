b548e541d355aaa723f239110f993e68ac525959
Rich Fecher <rfecher@gmail.com>
2017-04-28 11:27:01 -0400

    update rpm to use hbase 1.3.0

--- deploy/packaging/docker/docker-build-rpms.sh
+++ deploy/packaging/docker/docker-build-rpms.sh
@@ -45,7 +45,7 @@ if [ -z $BUILD_ARGS_MATRIX  ]; then
 	else
 		# Default build arguments
     	BUILD_ARGS_MATRIX=(
-	"-Daccumulo.version=1.7.2 -Daccumulo.api=1.7 -Dhadoop.version=2.7.3 -Dgeotools.version=16.0 -Dgeoserver.version=2.10.0 -Dhbase.version=1.2.3 -Dvendor.version=apache"
+	"-Daccumulo.version=1.7.2 -Daccumulo.api=1.7 -Dhadoop.version=2.7.3 -Dgeotools.version=16.0 -Dgeoserver.version=2.10.0 -Dhbase.version=1.3.0 -Dvendor.version=apache"
 	"-Daccumulo.version=1.7.2-cdh5.5.0 -Daccumulo.api=1.7 -Dhadoop.version=2.6.0-cdh5.9.0 -Dgeotools.version=16.0 -Dgeoserver.version=2.10.0 -Dhbase.version=1.2.0-cdh5.9.0 -P cloudera -Dvendor.version=cdh5"
 	"-Daccumulo.version=1.7.0.2.4.2.0-258 -Daccumulo.api=1.7 -Dhadoop.version=2.7.1.2.4.2.0-258 -Dgeotools.version=16.0 -Dgeoserver.version=2.10.0 -Dhbase.version=1.1.2.2.4.2.0-258 -P hortonworks -Dvendor.version=hdp2"
     	)
