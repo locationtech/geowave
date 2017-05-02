341691bde525d964f1ed44dfbb3f81569abbd1a9
Kent Miller <kmiller@radiantblue.com>
2017-05-02 12:45:18 -0700

    Proof-of-concept for twitter access

---
+++ extensions/formats/twitter/src/main/java/mil/nga/giat/geowave/format/twitter/stream/TwitterStreamArchiver.java
@@ -0,0 +1,104 @@
+package mil.nga.giat.geowave.format.twitter.stream;
+
+import com.twitter.hbc.ClientBuilder;
+import com.twitter.hbc.core.Constants;
+import com.twitter.hbc.core.endpoint.StatusesSampleEndpoint;
+import com.twitter.hbc.core.processor.StringDelimitedProcessor;
+import com.twitter.hbc.httpclient.BasicClient;
+import com.twitter.hbc.httpclient.auth.Authentication;
+import com.twitter.hbc.httpclient.auth.OAuth1;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+public class TwitterStreamArchiver
+{
+
+	public static void run(
+			String consumerKey,
+			String consumerSecret,
+			String token,
+			String secret )
+			throws InterruptedException {
+		// Create an appropriately sized blocking queue
+		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(
+				10000);
+
+		// Define our endpoint: By default, delimited=length is set (we need
+		// this for our processor)
+		// and stall warnings are on.
+		StatusesSampleEndpoint endpoint = new StatusesSampleEndpoint();
+		endpoint.stallWarnings(
+				false);
+
+		Authentication auth = new OAuth1(
+				consumerKey,
+				consumerSecret,
+				token,
+				secret);
+		// Authentication auth = new
+		// com.twitter.hbc.httpclient.auth.BasicAuth(username, password);
+
+		// Create a new BasicClient. By default gzip is enabled.
+		BasicClient client = new ClientBuilder()
+				.name(
+						"sampleExampleClient")
+				.hosts(
+						Constants.STREAM_HOST)
+				.endpoint(
+						endpoint)
+				.authentication(
+						auth)
+				.processor(
+						new StringDelimitedProcessor(
+								queue))
+				.build();
+
+		// Establish a connection
+		client.connect();
+
+		// Do whatever needs to be done with messages
+		for (int msgRead = 0; msgRead < 1000; msgRead++) {
+			if (client.isDone()) {
+				System.out.println(
+						"Client connection closed unexpectedly: " + client.getExitEvent().getMessage());
+				break;
+			}
+
+			String msg = queue.poll(
+					5,
+					TimeUnit.SECONDS);
+			if (msg == null) {
+				System.out.println(
+						"Did not receive a message in 5 seconds");
+			}
+			else {
+				System.out.println(
+						msg);
+			}
+		}
+
+		client.stop();
+
+		// Print some stats
+		System.out.printf(
+				"The client read %d messages!\n",
+				client.getStatsTracker().getNumMessages());
+	}
+
+	public static void main(
+			String[] args ) {
+		try {
+			TwitterStreamArchiver.run(
+					args[0],
+					args[1],
+					args[2],
+					args[3]);
+		}
+		catch (InterruptedException e) {
+			System.out.println(
+					e);
+		}
+	}
+}
---
+++ extensions/formats/twitter/src/test/java/mil/nga/giat/geowave/format/twitter/TwitterStreamArchiverTest.java
@@ -0,0 +1,46 @@
+package mil.nga.giat.geowave.format.twitter;
+
+import java.io.File;
+import java.util.Properties;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import mil.nga.giat.geowave.core.cli.operations.config.options.ConfigOptions;
+import mil.nga.giat.geowave.format.twitter.stream.TwitterStreamArchiver;
+
+public class TwitterStreamArchiverTest
+{
+	private static final String TWITTER_CONFIG = "src/test/resources/twitter-config.properties";
+	private String consumerKey;
+	private String consumerSecret;
+	private String accessToken;
+	private String accessSecret;
+	
+	@Before
+	public void setup() {
+		Properties twitterProps = ConfigOptions.loadProperties(
+				new File(TWITTER_CONFIG),
+				null);
+		
+		consumerKey = twitterProps.getProperty("twitter.consumer.key");
+		consumerSecret = twitterProps.getProperty("twitter.consumer.secret");
+		accessToken = twitterProps.getProperty("twitter.access.token");
+		accessSecret = twitterProps.getProperty("twitter.access.secret");
+	}
+	
+	@Test
+	public void testStreamArchiver() {
+		try {
+			TwitterStreamArchiver.run(
+					consumerKey,
+					consumerSecret,
+					accessToken,
+					accessSecret);
+		}
+		catch (InterruptedException e) {
+			System.out.println(
+					e);
+		}
+	}
+}
