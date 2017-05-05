8cae220db0e2ad5c6de7597e8f54525c55464d8f
rfecher <rfecher@gmail.com>
2017-05-05 16:31:45 -0400

    Adding a few security-related fixes and enhancements. Biggest ones in… (#1092)
    * Adding a few security-related fixes and enhancements. Biggest ones involve token key and props files not generating off the bat when a command is first run before any other commands, as well as updating the cryptography to utilize bouncycastle, which is not dependent on the JCE libraries being installed on a system in order to run.
    
    * reverted to using console for password prompt

--- core/cli/src/main/java/mil/nga/giat/geowave/core/cli/converters/PasswordConverter.java
+++ core/cli/src/main/java/mil/nga/giat/geowave/core/cli/converters/PasswordConverter.java
@@ -8,9 +8,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
 import com.beust.jcommander.ParameterException;
 
 import mil.nga.giat.geowave.core.cli.utils.FileUtils;
