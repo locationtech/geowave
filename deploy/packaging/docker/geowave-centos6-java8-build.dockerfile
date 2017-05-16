e142906cb23392389ac3e91be6a769fa2d477737
Rich Fecher <rfecher@gmail.com>
2017-05-16 15:12:36 -0400

    # This is a combination of 5 commits. # This is the 1st commit message: # This is a combination of 5 commits. # This is the 1st commit message: managed jackson-core-asl dependency to override cloudera
    # The commit message #2 will be skipped:
    
    #	Added script that will query the s3 bucket and search under dev*. Need to know what items to remove now
    
    # The commit message #3 will be skipped:
    
    #	adding defaults for the config-properties file
    
    # The commit message #4 will be skipped:
    
    #	updated mvn cache for packaging scripts
    
    # The commit message #5 will be skipped:
    
    #	added example for minicluster in cli
    
    # The commit message #6 will be skipped:
    
    #	Updated to latest version of jdk
    
    # The commit message #7 will be skipped:
    
    #	Updated jenkins and added clean up script
    
    # The commit message #8 will be skipped:
    
    #	Updated jenkinsfile
    
    # The commit message #9 will be skipped:
    
    #	Updated repo
    
    # The commit message #10 will be skipped:
    
    #	created clean script to manage s3 space
    
    # The commit message #1 will be skipped:
    
    #	updated package scripts to just use default versions per vendor
    
    # The commit message #2 will be skipped:
    
    #	Updated jenkinsfile and updated clean-bucket script
    
    # The commit message #3 will be skipped:
    
    #	uncommented s3 sync
    
    # The commit message #4 will be skipped:
    
    #	Fixed syntax error
    
    # The commit message #5 will be skipped:
    
    #	Merged clean-dir into clean-bucket and updated jenkinsfile to do cleaning before createrepo
    
    # The commit message #6 will be skipped:
    
    #	Updated interpreter path
    
    # The commit message #7 will be skipped:
    
    #	Added sudo to clean command
    
    # The commit message #8 will be skipped:
    
    #	rewrote GeoWaveRasterReaderState
    
    # The commit message #9 will be skipped:
    
    #	Added checkout scm
    
    # The commit message #10 will be skipped:
    
    #	Commented out cleanup script to not conflict with the other builds
    
    # The commit message #1 will be skipped:
    
    #	Bringing back in cleaning function
    
    # The commit message #2 will be skipped:
    
    #	Updated repo
    
    # The commit message #3 will be skipped:
    
    #	Updated node
    
    # The commit message #4 will be skipped:
    
    #	Removed sudo
    
    # The commit message #5 will be skipped:
    
    #	added test touch
    
    # The commit message #6 will be skipped:
    
    #	Removed test
    
    # The commit message #7 will be skipped:
    
    #	Removed sudo
    
    # The commit message #8 will be skipped:
    
    #	checking user
    
    # The commit message #9 will be skipped:
    
    #	sh
    
    # The commit message #10 will be skipped:
    
    #	Update Jenkinsfile
    
    # The commit message #1 will be skipped:
    
    #	Removed debug statements
    
    # The commit message #2 will be skipped:
    
    #	cleaned output statment
    
    # The commit message #3 will be skipped:
    
    #	Added exception handling on file search
    
    # The commit message #4 will be skipped:
    
    #	Updated output messages
    
    # The commit message #5 will be skipped:
    
    #	Changed SCM command back to pull from ngageoint
    
    # The commit message #6 will be skipped:
    
    #	updated repo
    
    # The commit message #7 will be skipped:
    
    #	Create Jenkinsfile
    
    # The commit message #8 will be skipped:
    
    #	Create Jenkinsfile
    
    # The commit message #9 will be skipped:
    
    #	Create Jenkinsfile
    
    # The commit message #10 will be skipped:
    
    #	Updated links from ngageoint to locationtech
    
    # The commit message #1 will be skipped:
    
    #	Added checks for geowave-repo* so that this file will not be removed from local or bucket
    
    # The commit message #2 will be skipped:
    
    #	Removed clean from jenkinsfile to test webhooks
    
    # The commit message #3 will be skipped:
    
    #	Extract only the specified attribute.s geometry info
    
    # The commit message #4 will be skipped:
    
    #	Create Jenkinsfile
    
    # The commit message #5 will be skipped:
    
    #	Uncommented Stage decleration
    
    # The commit message #2 will be skipped:
    
    #	Create Jenkinsfile
    
    # The commit message #3 will be skipped:
    
    #	changed branch
    
    # The commit message #4 will be skipped:
    
    #	changed checkout command
    
    # The commit message #5 will be skipped:
    
    #	Changed checkout

--- deploy/packaging/docker/geowave-centos6-java8-build.dockerfile
+++ deploy/packaging/docker/geowave-centos6-java8-build.dockerfile
@@ -4,8 +4,8 @@ RUN yum -y install asciidoc boost boost-devel gcc-c++ git glibc.i686 tar unzip w
 	yum clean all
 
 RUN cd /tmp && wget --no-check-certificate --no-cookies \
-	--header "Cookie: oraclelicense=accept-securebackup-cookie"  \
-	http://download.oracle.com/otn-pub/java/jdk/8u60-b27/jdk-8u60-linux-x64.rpm -q && \
+        --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+        http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm && \
 	rpm -Uvh /tmp/*.rpm && rm -fr /tmp/*.rpm && \
 	wget http://archive.apache.org/dist/maven/binaries/apache-maven-3.2.2-bin.zip && \
 	unzip apache-maven-3.2.2-bin.zip && \
--- extensions/adapters/raster/src/main/java/mil/nga/giat/geowave/adapter/raster/plugin/GeoWaveRasterReaderState.java
+++ extensions/adapters/raster/src/main/java/mil/nga/giat/geowave/adapter/raster/plugin/GeoWaveRasterReaderState.java
@@ -1,69 +1,86 @@
-/*
- *    GeoTools - The Open Source Java GIS Toolkit
- *    http://geotools.org
- *
- *    (C) 2008, Open Source Geospatial Foundation (OSGeo)
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License as published by the Free Software Foundation;
- *    version 2.1 of the License.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- */
-
+/*******************************************************************************
+ * Copyright (c) 2013-2017 Contributors to the Eclipse Foundation
+ * 
+ * See the NOTICE file distributed with this work for additional
+ * information regarding copyright ownership.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Apache License,
+ * Version 2.0 which accompanies this distribution and is available at
+ * http://www.apache.org/licenses/LICENSE-2.0.txt
+ ******************************************************************************/
 package mil.nga.giat.geowave.adapter.raster.plugin;
 
 import org.geotools.geometry.GeneralEnvelope;
 
 /**
- * Holds the state of the reader making it thread safe
+ * This class allows us to make the GeoWaveRasterReader thread safe by storing
+ * its state here
  */
-
 public class GeoWaveRasterReaderState
 {
-	private boolean xAxisSwitch = false;
 	private final String coverageName;
+	private boolean axisSwap = false;
 	private GeneralEnvelope requestedEnvelope = null;
-	private GeneralEnvelope requestEnvelopeTransformed = null;
+	private GeneralEnvelope requestEnvelopeXformed;
 
 	public GeoWaveRasterReaderState(
 			final String coverageName ) {
 		this.coverageName = coverageName;
 	}
 
+	/**
+	 * @return the coverageName
+	 */
 	public String getCoverageName() {
 		return coverageName;
 	}
 
+	/**
+	 * @return the boolean value of axisSwap
+	 */
+	public boolean isAxisSwapped() {
+		return axisSwap;
+	}
+
+	/**
+	 * @param axisSwap
+	 *            the boolean value to set
+	 */
+	public void setAxisSwap(
+			boolean axisSwap ) {
+		this.axisSwap = axisSwap;
+	}
+
+	/**
+	 * @return the requestedEnvelope
+	 */
 	public GeneralEnvelope getRequestedEnvelope() {
 		return requestedEnvelope;
 	}
 
+	/**
+	 * @param requestedEnvelope
+	 *            the requestedEnvelope to set
+	 */
 	public void setRequestedEnvelope(
-			final GeneralEnvelope requestedEnvelope ) {
+			GeneralEnvelope requestedEnvelope ) {
 		this.requestedEnvelope = requestedEnvelope;
 	}
 
-	public GeneralEnvelope getRequestEnvelopeTransformed() {
-		return requestEnvelopeTransformed;
+	/**
+	 * @return the requestEnvelopeXformed
+	 */
+	public GeneralEnvelope getRequestEnvelopeXformed() {
+		return requestEnvelopeXformed;
 	}
 
-	public void setRequestEnvelopeTransformed(
-			final GeneralEnvelope requestEnvelopeTransformed ) {
-		this.requestEnvelopeTransformed = requestEnvelopeTransformed;
-	}
-
-	public boolean isXAxisSwitch() {
-		return xAxisSwitch;
-	}
-
-	public void setXAxisSwitch(
-			final boolean axisSwitch ) {
-		xAxisSwitch = axisSwitch;
+	/**
+	 * @param requestEnvelopeXformed
+	 *            the requestEnvelopeXformed to set
+	 */
+	public void setRequestEnvelopeXformed(
+			GeneralEnvelope requestEnvelopeXformed ) {
+		this.requestEnvelopeXformed = requestEnvelopeXformed;
 	}
 
 }
