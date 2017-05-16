0b96e4d2965cff10609ff98ac0e3be3f2def98c6
Kent Miller <kmiller@radiantblue.com>
2017-05-16 09:31:23 -0700

    handle multi-part tweets files

--- extensions/formats/twitter/src/test/resources/tweets2s3.sh
+++ extensions/formats/twitter/src/test/resources/tweets2s3.sh
@@ -7,14 +7,15 @@ pushd $archivePath
 today=$(date +%Y%m%d)
 echo Today is $today
 
-currentTweets=tweets-$today.json
-echo Making sure we omit the current tweet file: $currentTweets
+currentTweetsP1=tweets-${today}p1.json
+currentTweetsP2=tweets-${today}p2.json
+echo Making sure we omit the current tweet files: $currentTweetsP1, $currentTweetsP2
 
 # Find any old tweet files
-for f in tweets-*.json
+for f in tweets-*p*.json
 do
 	echo Processing $f...
-	if [ "$f" != "$currentTweets" ]; then
+	if [ "$f" != "$currentTweetsP1" ] && [ "$f" != "$currentTweetsP2" ]; then
 		# Zip it up
 		gzip $f
 		zippedFile=$f.gz
@@ -33,3 +34,4 @@ do
 done
 
 echo Done!
+
