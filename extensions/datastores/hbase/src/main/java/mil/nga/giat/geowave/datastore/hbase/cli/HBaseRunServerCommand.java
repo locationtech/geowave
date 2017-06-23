03b3e7063c46f640bff15a1e9084021fcc0ed4cc
Rich Fecher <rfecher@gmail.com>
2017-06-23 14:15:28 -0400

    moved test server commands from examples to projects

--- extensions/datastores/hbase/src/main/java/mil/nga/giat/geowave/datastore/hbase/cli/HBaseRunServerCommand.java
+++ extensions/datastores/hbase/src/main/java/mil/nga/giat/geowave/datastore/hbase/cli/HBaseRunServerCommand.java
@@ -1,4 +1,4 @@
-package mil.nga.giat.geowave.example.cli;
+package mil.nga.giat.geowave.datastore.hbase.cli;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,15 +9,14 @@ import mil.nga.giat.geowave.core.cli.annotations.GeowaveOperation;
 import mil.nga.giat.geowave.core.cli.api.Command;
 import mil.nga.giat.geowave.core.cli.api.DefaultOperation;
 import mil.nga.giat.geowave.core.cli.api.OperationParams;
-import mil.nga.giat.geowave.examples.hbase.HBaseMiniCluster;
 
-@GeowaveOperation(name = "hbase-server", parentOperation = ExampleSection.class)
+@GeowaveOperation(name = "runserver", parentOperation = HBaseSection.class)
 @Parameters(commandDescription = "Runs a standalone mini HBase server for test and debug with GeoWave")
-public class ExampleHBaseServerCommand extends
+public class HBaseRunServerCommand extends
 		DefaultOperation implements
 		Command
 {
-	private static final Logger LOGGER = LoggerFactory.getLogger(ExampleHBaseServerCommand.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(HBaseRunServerCommand.class);
 
 	/**
 	 * Prep the driver & run the operation.
