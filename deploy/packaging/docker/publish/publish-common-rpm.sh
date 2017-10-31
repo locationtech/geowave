01ed649e0acd2ea45bb183c04bf81a18faba4dba
John Wileczek <john.wileczek@prominentedge.com>
2017-10-31 09:02:47 -0400

    Updating documentation (+6 squashed commits) Squashed commits: [3667f53] Updating notebook and removing previous scaled time changes [6edb87c] Update bootstrap scripts to better handle the new Upstart service, and maximizeResourceAllocation for spark [911c069] Removing unused scaledTimeRange [db0d110] Update jupyter kernel creation script to add Jupyter to Upstart so it can be run at emr cluster creation. [d6c10f7] Update driver memory settings and small fix to cli options. [5b980ab] Updating jupyter notebooks and putting all example notebooks on s3

--- deploy/packaging/docker/publish/publish-common-rpm.sh
+++ deploy/packaging/docker/publish/publish-common-rpm.sh
@@ -70,8 +70,7 @@ if command -v aws >/dev/null 2>&1 ; then
 		aws s3 cp --acl public-read --recursive ${WORKSPACE}/deploy/packaging/emr/generated/ s3://geowave/${GEOWAVE_VERSION_URL}/scripts/emr/
 		aws s3 cp --acl public-read --recursive ${WORKSPACE}/deploy/packaging/sandbox/generated/ s3://geowave/${GEOWAVE_VERSION_URL}/scripts/sandbox/
 		
-		# Adding copy for example jupyter notebooks to s3 bucket. TODO: Will only use GDELT for now (gpx notebook not ready) should be entire notebook directory copy
-		aws s3 cp --acl public-read ${WORKSPACE}/examples/data/notebooks/jupyter/geowave-gdelt.ipynb s3://geowave-notebooks/${GEOWAVE_VERSION_URL}/notebooks/geowave-gdelt.ipynb
+		aws s3 cp --acl public-read --recursive ${WORKSPACE}/examples/data/notebooks/jupyter/ s3://geowave-notebooks/${GEOWAVE_VERSION_URL}/notebooks/
 	else
 		echo '###### Skipping publish to S3: GEOWAVE_VERSION_URL not defined'
 	fi
