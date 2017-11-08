0a0c871a4f9401821af414fc7622c1360c93c1a0
Ahmed Kamel <ahmed.kamel@digitalglobe.com>
2017-11-08 15:46:11 -0700

    Bug fixes

--- deploy/packaging/puppet/geowave/manifests/gwtomcat_server.pp
+++ deploy/packaging/puppet/geowave/manifests/gwtomcat_server.pp
@@ -11,7 +11,7 @@ class geowave::gwtomcat_server {
   package { "geowave-${geowave::geowave_version}-${geowave::hadoop_vendor_version}-gwtomcat":
     ensure => latest,
     tag    => 'geowave-package',
-    notify  => Service['gwtomcat']
+    notify  => Service['gwtomcat'],
   }
 
   file_line {'Change_default_port':
@@ -19,6 +19,7 @@ class geowave::gwtomcat_server {
     path    => "/usr/local/geowave-${geowave::geowave_version}-${geowave::hadoop_vendor_version}/tomcat8/conf/server.xml",
     line    => "<Connector port=\"${http_port}\" protocol=\"HTTP/1.1\"",
     match   => '.Connector\ port="(\d{1,5}".protocol="HTTP.*"$)',
-    notify  => Service['gwtomcat']
+    require => Package["geowave-${geowave::geowave_version}-${geowave::hadoop_vendor_version}-gwtomcat"],
+    notify  => Service['gwtomcat'],
   }
 }
