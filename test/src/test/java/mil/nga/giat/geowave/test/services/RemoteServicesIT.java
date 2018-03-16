7b35b66ed169e3f8407290608f2f71186f140273
James Yarrington <jyarrington@vt.edu>
2018-03-16 16:14:49 -0400

    start remote test work, refactor status code assertion

--- test/src/test/java/mil/nga/giat/geowave/test/services/RemoteServicesIT.java
+++ test/src/test/java/mil/nga/giat/geowave/test/services/RemoteServicesIT.java
@@ -1,12 +1,16 @@
 package mil.nga.giat.geowave.test.services;
 
+import javax.ws.rs.core.Response;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import mil.nga.giat.geowave.core.store.operations.remote.options.DataStorePluginOptions;
+import mil.nga.giat.geowave.service.client.ConfigServiceClient;
 import mil.nga.giat.geowave.service.client.RemoteServiceClient;
 import mil.nga.giat.geowave.test.GeoWaveITRunner;
 import mil.nga.giat.geowave.test.TestUtils;
@@ -19,20 +23,24 @@ import mil.nga.giat.geowave.test.annotation.GeoWaveTestStore.GeoWaveStoreType;
 @Environments({
 	Environment.SERVICES
 })
-public class RemoteServicesIT {
-	
+public class RemoteServicesIT
+{
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(GeoServerServicesIT.class);
 	private static RemoteServiceClient remoteServiceClient;
-	
+	private static ConfigServiceClient configServiceClient;
+
 	@GeoWaveTestStore(value = {
-			GeoWaveStoreType.ACCUMULO,
-			GeoWaveStoreType.BIGTABLE,
-		    GeoWaveStoreType.HBASE
-		})
+		GeoWaveStoreType.ACCUMULO,
+		GeoWaveStoreType.BIGTABLE,
+		GeoWaveStoreType.HBASE
+	})
 	protected DataStorePluginOptions dataStorePluginOptions;
 
 	private static long startMillis;
 	private final static String testName = "RemoteServicesIT";
+
+	private String storeName = "store-under-remote-test";
 
 	@BeforeClass
 	public static void setup() {
@@ -41,6 +49,8 @@ public class RemoteServicesIT {
 		// GeoWaveServicesIT.class.getClassLoader().getResource(
 		// TEST_DATA_ZIP_RESOURCE_PATH).toURI()),
 		// TestUtils.TEST_CASE_BASE);
+		configServiceClient = new ConfigServiceClient(
+				ServicesTestEnvironment.GEOWAVE_BASE_URL);
 		remoteServiceClient = new RemoteServiceClient(
 				ServicesTestEnvironment.GEOWAVE_BASE_URL);
 		startMillis = System.currentTimeMillis();
@@ -56,6 +66,15 @@ public class RemoteServicesIT {
 				LOGGER,
 				testName,
 				startMillis);
+	}
+
+	@Test
+	public void simpleTest() {
+		Response r = remoteServiceClient.listStats(storeName);
+		TestUtils.assert404(
+				"Should not be able to find a non-existent store",
+				r.getStatus());
+		System.out.println(r.readEntity(String.class));
 	}
 
 }
