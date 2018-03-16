78aea3b3069a6d65daa458a9c06fbf5c9ce4a450
Eric McDonald <ejm2pe@virginia.edu>
2018-03-16 12:31:54 -0400

    updated response codes for gesoserver commands

--- extensions/cli/geoserver/src/main/java/mil/nga/giat/geowave/cli/geoserver/GeoServerRemoveDatastoreCommand.java
+++ extensions/cli/geoserver/src/main/java/mil/nga/giat/geowave/cli/geoserver/GeoServerRemoveDatastoreCommand.java
@@ -16,6 +16,9 @@ import java.util.List;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
 
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
+
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterException;
@@ -23,6 +26,7 @@ import com.beust.jcommander.Parameters;
 
 import mil.nga.giat.geowave.core.cli.annotations.GeowaveOperation;
 import mil.nga.giat.geowave.core.cli.api.OperationParams;
+import mil.nga.giat.geowave.core.cli.api.ServiceStatus;
 
 @GeowaveOperation(name = "rmds", parentOperation = GeoServerSection.class)
 @Parameters(commandDescription = "Remove GeoServer DataStore")
@@ -39,12 +43,29 @@ public class GeoServerRemoveDatastoreCommand extends
 	private final List<String> parameters = new ArrayList<String>();
 	private String datastoreName = null;
 
+	private ServiceStatus status = ServiceStatus.OK;
+
 	@Override
 	public void execute(
 			final OperationParams params )
 			throws Exception {
 		JCommander.getConsole().println(
 				computeResults(params));
+	}
+
+	public void setStatus(
+			ServiceStatus status ) {
+		this.status = status;
+	}
+
+	@Override
+	public Pair<ServiceStatus, String> executeService(
+			OperationParams params )
+			throws Exception {
+		String ret = computeResults(params);
+		return ImmutablePair.of(
+				status,
+				ret);
 	}
 
 	@Override
@@ -67,7 +88,18 @@ public class GeoServerRemoveDatastoreCommand extends
 				datastoreName);
 
 		if (deleteStoreResponse.getStatus() == Status.OK.getStatusCode()) {
-			return "Delete store '" + datastoreName + "' from workspace '" + workspace + "' on GeoServer: OK";
+			setStatus(ServiceStatus.OK);
+		}
+		switch (deleteStoreResponse.getStatus()) {
+			case 404:
+				setStatus(ServiceStatus.NOT_FOUND);
+				break;
+			case 400:
+				setStatus(ServiceStatus.DUPLICATE);
+				break;
+			default:
+				setStatus(ServiceStatus.INTERNAL_ERROR);
+				break;
 		}
 		return "Error deleting store '" + datastoreName + "' from workspace '" + workspace + "' on GeoServer; code = "
 				+ deleteStoreResponse.getStatus();
