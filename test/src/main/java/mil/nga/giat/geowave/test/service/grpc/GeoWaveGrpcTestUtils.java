3c6e36795c221d4359219bd5d946edcd11438914
Rich Fecher <rfecher@gmail.com>
2018-07-24 11:08:41 -0400

    fixed date format

--- test/src/main/java/mil/nga/giat/geowave/test/service/grpc/GeoWaveGrpcTestUtils.java
+++ test/src/main/java/mil/nga/giat/geowave/test/service/grpc/GeoWaveGrpcTestUtils.java
@@ -15,8 +15,8 @@ public class GeoWaveGrpcTestUtils
 	public final static String cqlSpatialQuery = "BBOX(geometry,0.0,0.0, 25.0, 25.0)";
 	public final static String wktSpatialQuery = "POLYGON (( " + "0.0 0.0, " + "0.0 25.0, " + "25.0 25.0, "
 			+ "25.0 0.0, " + "0.0 0.0" + "))";
-	public final static String temporalQueryStartTime = "2016-02-20T01:32:56Z";
-	public final static String temporalQueryEndTime = "2016-02-21T01:32:56Z";
+	public final static String temporalQueryStartTime = "2016-02-20T01:32Z";
+	public final static String temporalQueryEndTime = "2016-02-21T01:32Z";
 
 	// this is purely a convenience method so the gRPC test client does not need
 	// any dependency on the test environment directly
