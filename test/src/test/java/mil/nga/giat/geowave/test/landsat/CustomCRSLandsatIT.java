9882b76c430d42d96004aa49cda4c4e8c9cfd9da
Rich Fecher <rfecher@gmail.com>
2018-09-04 12:32:42 -0400

    changed bbox for landsat IT

--- test/src/test/java/mil/nga/giat/geowave/test/landsat/CustomCRSLandsatIT.java
+++ test/src/test/java/mil/nga/giat/geowave/test/landsat/CustomCRSLandsatIT.java
@@ -121,10 +121,10 @@ public class CustomCRSLandsatIT extends
 	private static final int MAX_PATH = 199;
 	private static final int MIN_ROW = 36;
 	private static final int MAX_ROW = 36;
-	private static final double WEST = -2.4;
-	private static final double EAST = -1.9;
-	private static final double NORTH = 34.5;
-	private static final double SOUTH = 34;
+	private static final double WEST = -2.2;
+	private static final double EAST = -1.7;
+	private static final double NORTH = 34.3;
+	private static final double SOUTH = 33.8;
 
 	private final static Logger LOGGER = LoggerFactory.getLogger(CustomCRSLandsatIT.class);
 	private static long startMillis;
