03dd1e7e97c4c7fd15bbd726de54af648e7770a4
Danny Qiu <dq29@cornell.edu>
2019-02-10 11:33:54 -0500

    Add tests for rocksdb datastore (#1498)
    * Add tests for rocksdb metadata table
    
    Signed-off-by: Danny Qiu <dqiu55@gmail.com>
    
    * Add tests for rocksdb utils

---
+++ extensions/datastores/rocksdb/src/test/java/org/locationtech/geowave/datastore/rocksdb/util/RocksDBUtilsTest.java
@@ -0,0 +1,18 @@
+package org.locationtech.geowave.datastore.rocksdb.util;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class RocksDBUtilsTest {
+
+    @Test
+    public void testGetTableName() {
+        String nullPartitionKeyName = RocksDBUtils.getTableName("prefix-null", null);
+        assertEquals("prefix-null", nullPartitionKeyName);
+
+        String emptyPartitionKeyName = RocksDBUtils.getTableName("prefix-empty", new byte[] {});
+        assertEquals("prefix-empty", emptyPartitionKeyName);
+    }
+
+}
