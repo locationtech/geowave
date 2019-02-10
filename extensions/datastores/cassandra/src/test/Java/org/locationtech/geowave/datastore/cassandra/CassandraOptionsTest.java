d897fb85b3950d0c38cb07235f4419578c491d59
Nik Gil <nikmanG@users.noreply.github.com>
2019-02-10 16:26:01 -0500

    Python bindings (#1515)
    * updated cassandra test (#1512)
    
    * merge with master

--- extensions/datastores/cassandra/src/test/Java/org/locationtech/geowave/datastore/cassandra/CassandraOptionsTest.java
+++ extensions/datastores/cassandra/src/test/Java/org/locationtech/geowave/datastore/cassandra/CassandraOptionsTest.java
@@ -9,45 +9,45 @@ import org.locationtech.geowave.datastore.cassandra.config.CassandraOptions;
 
 public class CassandraOptionsTest {
 
-    final private int batchWriteSize = 10;
-    private CassandraOptions mockOptions;
-    final private boolean durableSize = true;
-    final private int replicationFactor = 20;
+  final private int batchWriteSize = 10;
+  private CassandraOptions mockOptions;
+  final private boolean durableSize = true;
+  final private int replicationFactor = 20;
 
-    @Before
-    public void setup() {
-        mockOptions = new CassandraOptions();
-    }
+  @Before
+  public void setup() {
+    mockOptions = new CassandraOptions();
+  }
 
-    @After
-    public void cleanup() {
-        mockOptions = null;
-    }
+  @After
+  public void cleanup() {
+    mockOptions = null;
+  }
 
-    @Test
-    public void testSetBatchWriteSize() {
-        mockOptions.setBatchWriteSize(batchWriteSize);
-        int size = mockOptions.getBatchWriteSize();
-        Assert.assertEquals(batchWriteSize, size);
-    }
+  @Test
+  public void testSetBatchWriteSize() {
+    mockOptions.setBatchWriteSize(batchWriteSize);
+    int size = mockOptions.getBatchWriteSize();
+    Assert.assertEquals(batchWriteSize, size);
+  }
 
-    @Test
-    public void testSetDurableWrites() {
-        mockOptions.setDurableWrites(durableSize);
-        boolean isDurable = mockOptions.isDurableWrites();
-        Assert.assertTrue(isDurable);
-    }
+  @Test
+  public void testSetDurableWrites() {
+    mockOptions.setDurableWrites(durableSize);
+    boolean isDurable = mockOptions.isDurableWrites();
+    Assert.assertTrue(isDurable);
+  }
 
-    @Test
-    public void testIsServerSideLibraryEnabled() {
-        boolean isServerEnabled = mockOptions.isServerSideLibraryEnabled();
-        Assert.assertFalse(isServerEnabled);
-    }
+  @Test
+  public void testIsServerSideLibraryEnabled() {
+    boolean isServerEnabled = mockOptions.isServerSideLibraryEnabled();
+    Assert.assertFalse(isServerEnabled);
+  }
 
-    @Test
-    public void testSetReplicationFactor() {
-        mockOptions.setReplicationFactor(replicationFactor);
-        int getRF = mockOptions.getReplicationFactor();
-        Assert.assertEquals(replicationFactor, getRF);
-    }
+  @Test
+  public void testSetReplicationFactor() {
+    mockOptions.setReplicationFactor(replicationFactor);
+    int getRF = mockOptions.getReplicationFactor();
+    Assert.assertEquals(replicationFactor, getRF);
+  }
 }
