aa4da1c3e9f59c9e74bfbb6db031ddb19c395a2f
carolyntang <carolyntang1129@gmail.com>
2019-02-10 11:32:06 -0500

    Add unit test cases for Cassandra datastore (#1496)
    Signed-off-by: carolyntang <carolyntang1129@gmail.com>

---
+++ extensions/datastores/cassandra/src/test/Java/org/locationtech/geowave/datastore/cassandra/CassandraRequiredOptionsTest.java
@@ -0,0 +1,23 @@
+package org.locationtech.geowave.datastore.cassandra;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.locationtech.geowave.datastore.cassandra.config.CassandraRequiredOptions;
+
+public class CassandraRequiredOptionsTest {
+    private CassandraRequiredOptions mockRequiredOptions;
+    final private String contactPoint = "contactPointTest";
+
+    @Before
+    public void setup() {
+        mockRequiredOptions = new CassandraRequiredOptions();
+    }
+
+    @Test
+    public void testSetContactPoint() {
+        mockRequiredOptions.setContactPoint(contactPoint);
+        String getCT = mockRequiredOptions.getContactPoint();
+        Assert.assertEquals(contactPoint, getCT);
+    }
+}
