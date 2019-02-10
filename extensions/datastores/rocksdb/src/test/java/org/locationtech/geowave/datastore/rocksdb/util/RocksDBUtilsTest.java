bbf31c5d936147b95f6e1b92cb57ed39219116f9
Rich Fecher <rfecher@gmail.com>
2019-02-10 14:51:18 -0500

    updated cassandra test

--- extensions/datastores/rocksdb/src/test/java/org/locationtech/geowave/datastore/rocksdb/util/RocksDBUtilsTest.java
+++ extensions/datastores/rocksdb/src/test/java/org/locationtech/geowave/datastore/rocksdb/util/RocksDBUtilsTest.java
@@ -1,18 +1,17 @@
 package org.locationtech.geowave.datastore.rocksdb.util;
 
 import static org.junit.Assert.assertEquals;
-
 import org.junit.Test;
 
 public class RocksDBUtilsTest {
 
-    @Test
-    public void testGetTableName() {
-        String nullPartitionKeyName = RocksDBUtils.getTableName("prefix-null", null);
-        assertEquals("prefix-null", nullPartitionKeyName);
+  @Test
+  public void testGetTableName() {
+    String nullPartitionKeyName = RocksDBUtils.getTableName("prefix-null", null);
+    assertEquals("prefix-null", nullPartitionKeyName);
 
-        String emptyPartitionKeyName = RocksDBUtils.getTableName("prefix-empty", new byte[] {});
-        assertEquals("prefix-empty", emptyPartitionKeyName);
-    }
+    String emptyPartitionKeyName = RocksDBUtils.getTableName("prefix-empty", new byte[] {});
+    assertEquals("prefix-empty", emptyPartitionKeyName);
+  }
 
 }
