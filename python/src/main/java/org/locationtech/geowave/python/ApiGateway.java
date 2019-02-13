a40297ddc9e6287ba11b04ef21ca95dbf7558358
Nik Gil <nikmanG@users.noreply.github.com>
2019-02-13 12:28:54 -0500

    Python bindings (#1516)
    * updated cassandra test (#1512)
    
    * merge with master
    
    * bare minimum API Java-side

--- python/src/main/java/org/locationtech/geowave/python/ApiGateway.java
+++ python/src/main/java/org/locationtech/geowave/python/ApiGateway.java
@@ -1,5 +1,27 @@
 package org.locationtech.geowave.python;
 
+import org.locationtech.geowave.core.store.api.DataStore;
+import org.locationtech.geowave.core.store.api.DataStoreFactory;
+import org.locationtech.geowave.datastore.rocksdb.config.RocksDBOptions;
+import py4j.GatewayServer;
+
 public class ApiGateway {
-  // Some starter code
+    // Some starter code
+    public DataStore getRocksDB(String namespace, String dir) {
+        RocksDBOptions ops = new RocksDBOptions(namespace);
+
+        ops.setDirectory(dir);
+
+        return DataStoreFactory.createDataStore(ops);
+    }
+
+    public int getSize(DataStore d) {
+        return d.getIndices().length;
+    }
+
+    public static void main(String[] args) {
+        GatewayServer server = new GatewayServer(new ApiGateway());
+
+        server.start();
+    }
 }
