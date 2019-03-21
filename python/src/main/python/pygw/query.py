ffa500aee8b4a83554a2131cd2cbd144a4d58b05
Charles Yu <charles.yu108@gmail.com>
2019-03-21 15:22:56 -0400

    Basic query implementation

--- python/src/main/python/pygw/query.py
+++ python/src/main/python/pygw/query.py
@@ -1,10 +1,10 @@
 from pygw.config import config
-from pygw.base_models import QueryBuilderInterface, QueryInterface
+from pygw.base_models import PyGwJavaWrapper, QueryInterface
 
 class Query(QueryInterface):
     """ NOTE: This utilizes a factory pattern"""
 
-    def __init__(self, java_ref=None):
+    def __init__(self, java_ref):
         super().__init__(config.GATEWAY, java_ref)
 
     @classmethod
@@ -21,7 +21,7 @@ class Query(QueryInterface):
         if auths:
             qb.set_auth(auths)
         if limit:
-            qb.set_limits(limit)
+            qb.set_limit(limit)
         if type_names:
             qb.set_type_names(type_names)
         if which_fields:
@@ -37,11 +37,9 @@ class Query(QueryInterface):
         """
         return cls.build()
 
-class QueryBuilder(QueryBuilderInterface):
+class QueryBuilder(PyGwJavaWrapper):
     """
-    NOTE: this might never have to be exposed to end-user 
-            - Question: should this be consolidated with query?
-
+    [INTERNAL]
     Necessary for a query:
     - Constraints (default: Everything)
     - Index: either all or single (default: All)
@@ -58,32 +56,86 @@ class QueryBuilder(QueryBuilderInterface):
     def set_type_names(self, type_names=None):
         if type_names:
             assert isinstance(type_names, list)
+            for t in type_names: assert isinstance(t, str)
             n = len(type_names)
             j_string_cls = config.GATEWAY.jvm.java.lang.String
             j_string_arr = config.GATEWAY.new_array(j_string_cls, n)
             for idx, name in enumerate(type_names):
                 j_string_arr[idx] = name
+            self.type_names = type_names
             self._java_ref.setTypeNames(j_string_arr)
 
     def set_constraint(self, constraint=None):
         if constraint:
-            pass
+            # NOTE: Might have to introduce a constraint maker. These are more complex than anticipated.
+            # For now, just creating the base constraints with no opts
+            try:
+                j_constraint = QueryBuilder.VALID_CONSTRAINTS[constraint]()
+                self._java_ref.constraints(j_constraint)
+            except KeyError:
+                raise QueryBuilder.InvalidConstraintError("No constraint named {}".format(constraint))
 
     def set_auth(self, auths=None):
         if auths:
-            pass
-    
-    def set_limits(self, limit=None):
+            assert isinstance(auths, list)
+            for a in auths: assert isinstance(a, str)
+            n = len(auths)
+            j_string_cls = config.GATEWAY.jvm.java.lang.String
+            j_string_arr = config.GATEWAY.new_array(j_string_cls, n)
+            for idx, auth in enumerate(auths):
+                j_string_arr[idx] = auth
+            self._java_ref.setAuthorizations(j_string_arr)
+            
+    def set_limit(self, limit=None):
         if limit:
-            pass
+            assert isinstance(limit, int)
+            self._java_ref.limit(limit)
     
     def set_fields(self, fields=None):
+        if not hasattr(self, 'type_names'):
+            raise QueryBuilder.IncompatibleOptions(\
+                 "Subset fields can only be applied when exactly ONE type name is set: You have 0 set.")
+        if len(self.type_names) != 1:
+            raise QueryBuilder.IncompatibleOptions(\
+                "Subset fields can only be applied when exactly ONE type name is set: You have {} set.".format(len(self.type_names)))
         if fields:
-            pass
-    
+            assert isinstance(fields, list)
+            for f in fields: assert isinstance(f, str)
+            n = len(fields)
+            j_string_cls = config.GATEWAY.jvm.java.lang.String
+            j_string_arr = config.GATEWAY.new_array(j_string_cls, n)
+            for idx, field in enumerate(fields):
+                j_string_arr[idx] = field
+            self._java_ref.subsetFields(self.type_names[0], j_string_arr)
+            
     def set_index(self, index=None):
         if index:
             self._java_ref.indexName(index)
 
     def build(self):
+        """Builds a query."""
         return self._java_ref.build()
+
+    # For now just doing those in query constraints
+    VALID_CONSTRAINTS = {
+        "everything" : lambda : config.MODULE__query_constraints.EverythingQuery(),
+
+        # TODO - Looks like constructor takes add'l opts ... Investigate
+        "basic" : lambda : config.MODULE__query_constraints.BasicQuery(),
+
+        # TODO - Looks like constructor takes some add'l opts
+        "coordinate_range" : lambda: config.MODULE__query_constraints.CoordinateRangeQuery(),
+
+        # TODO - Looks like constructor takes some add'l opts
+        "data_id" : lambda: config.MODULE__query_constraints.DataIdQuery(),
+
+        # TODO - Looks like constructor takes some add'l opts
+        "insertion_id" : lambda: config.MODULE__query_constraints.InsertionIdQuery(),
+
+         # TODO - Looks like constructor takes some add'l opts
+        "prefix_id" : lambda: config.MODULE__query_constraints.PrefixIdQuery(),
+    }
+
+    class InvalidConstraintError(Exception): pass
+    class IncompatibleOptions(Exception): pass
+    
