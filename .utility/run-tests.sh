6a1b7cdc92b98a1613d300d69d02b78c61b34dcd
Rich Fecher <rfecher@gmail.com>
2019-05-13 09:02:03 -0400

    travis publish to maven central

--- .utility/run-tests.sh
+++ .utility/run-tests.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -ev
-
-if [ "$IT_ONLY" == "true" ]; then
-  echo -e "Skipping unit tests w/ verify...\n"
-  mvn -q verify -am -pl test -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
-else
-  echo -e "Running unit tests only w/ verify...\n"
-  mvn -q verify -Dformatter.action=validate -P $MAVEN_PROFILES
+if [ "$BUILD_AND_PUBLISH" == "false" ]; then 
+  if [ "$IT_ONLY" == "true" ]; then
+    echo -e "Skipping unit tests w/ verify...\n"
+    mvn -q verify -am -pl test -Dtest=SkipUnitTests -DfailIfNoTests=false -P $MAVEN_PROFILES
+  else
+    echo -e "Running unit tests only w/ verify...\n"
+    mvn -q verify -Dformatter.action=validate -P $MAVEN_PROFILES
+  fi
 fi
