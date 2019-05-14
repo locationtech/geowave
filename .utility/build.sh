a6537d001b99ca6f2eaf261472a78f52bf44fe8f
rfecher <rfecher@gmail.com>
2019-05-14 16:43:49 -0400

    travis publish to maven central (#1559)

--- .utility/build.sh
+++ .utility/build.sh
@@ -3,11 +3,11 @@ set -ev
 
 if [ "$TRAVIS_REPO_SLUG" == "locationtech/geowave" ] && [ "$BUILD_AND_PUBLISH" == "true" ] && [ "$TRAVIS_BRANCH" == "master" ]; then
   echo -e "Building docs...\n"
-  mvn -P html -pl docs install
+  mvn -P html -pl docs install -DskipTests -Dspotbug.skip
   
   echo -e "Installing local artifacts...\n"
-  mvn -q install -DskipTests -Dfindbugs.skip
+  mvn -q install -DskipTests -Dspotbugs.skip
   
   echo -e "Building javadocs...\n"
-  mvn -q javadoc:aggregate
+  mvn -q javadoc:aggregate -DskipTests -Dspotbugs.skip
 fi
--- .utility/publish.sh
+++ .utility/publish.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_REPO_SLUG" == "locationtech/geowave" ] && [ "$BUILD_AND_PUBLISH" == "true" ] && [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_BRANCH" == "master" ]; then  
   echo $GPG_SECRET_KEYS | base64 --decode | $GPG_EXECUTABLE --import
   echo $GPG_OWNERTRUST | base64 --decode | $GPG_EXECUTABLE --import-ownertrust
-  mvn deploy --settings .utility/.maven.xml -DskipTests=true -Dspotbugs.skip -B -U -Prelease
+  mvn deploy --settings .utility/.maven.xml -DskipTests -Dspotbugs.skip -B -U -Prelease
   echo -e "Generating changelog...\n"
   export CHANGELOG_GITHUB_TOKEN=$GH_TOKEN
   gem install github_changelog_generator
