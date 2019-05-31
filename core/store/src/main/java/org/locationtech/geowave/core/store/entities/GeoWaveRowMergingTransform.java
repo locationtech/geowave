ce95c78c8274add690e3208a77fc35da5afa9e7e
Rich Fecher <rfecher@gmail.com>
2019-05-31 16:57:57 -0400

    wip

---
+++ core/store/src/main/java/org/locationtech/geowave/core/store/entities/GeoWaveRowMergingTransform.java
@@ -0,0 +1,38 @@
+package org.locationtech.geowave.core.store.entities;
+
+import java.io.IOException;
+import java.util.Iterator;
+import org.checkerframework.checker.nullness.qual.Nullable;
+import org.locationtech.geowave.core.store.adapter.RowMergingDataAdapter;
+import org.locationtech.geowave.core.store.adapter.RowMergingDataAdapter.RowTransform;
+import org.locationtech.geowave.core.store.util.DataStoreUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.google.common.collect.Iterators;
+
+public class GeoWaveRowMergingTransform implements GeoWaveRowIteratorTransformer<GeoWaveRow> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(GeoWaveRowMergingTransform.class);
+  private final RowTransform<?> rowTransform;
+
+  public GeoWaveRowMergingTransform(
+      final RowMergingDataAdapter<?, ?> adapter,
+      final short internalAdapterId) {
+    super();
+    rowTransform = adapter.getTransform();
+    try {
+      rowTransform.initOptions(adapter.getOptions(internalAdapterId, null));
+    } catch (final IOException e) {
+      LOGGER.warn("Unable to initialize row merging adapter for type: " + adapter.getTypeName(), e);
+    }
+  }
+
+  @Override
+  public @Nullable Iterator<GeoWaveRow> apply(@Nullable final Iterator<GeoWaveRow> input) {
+    if (input != null) {
+      return Iterators.transform(input, row -> {
+        return DataStoreUtils.mergeSingleRowValues(row, rowTransform);
+      });
+    }
+    return null;
+  }
+}
