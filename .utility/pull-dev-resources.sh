f003e3b31e4071a2a8dd856467386bc4260979db
Rich Fecher <rfecher@gmail.com>
2019-06-17 10:38:55 -0400

    wip

--- .utility/pull-dev-resources.sh
+++ .utility/pull-dev-resources.sh
@@ -1,31 +1,9 @@
 #!/bin/bash
 # Don't set -e in case the S3 pull fails
 set -v
-version=${DEV_RESOURCES_VERSION:=1.5}
-echo -e "Pull or build dev-resources-${version}.jar..."
-
-if [ "$TRAVIS_REPO_SLUG" == "locationtech/geowave" ]; then
-	# Pull dev-resources jar from S3
-	pushd dev-resources
-	if [ ! -d target ]; then
-		mkdir target
-	else
-		echo -e "target folder exists"
-	fi
-	pushd target
-	if [ ! -f geowave-dev-resources-${version}.jar ]; then
-		echo -e "Pulling dev-resources jar from S3...\n"
-		wget https://s3.amazonaws.com/geowave-maven/release/org/locationtech/geowave/geowave-dev-resources/${version}/geowave-dev-resources-${version}.jar
-	else
-		echo -e "dev-resources jar exists"
-	fi
-	popd
-	if [ -f target/geowave-dev-resources-${version}.jar ]; then
-		# Install downloaded jar to local repo
-		mvn install:install-file -Dfile=target/geowave-dev-resources-${version}.jar -DgroupId=org.locationtech -DartifactId=geowave-dev-resources -Dversion=${version} -Dpackaging=jar
-	else
-		# Fallback to building the jar if the S3 pull failed
-		echo -e "Pull from S3 failed. Building..."
-		mvn clean install
-	fi
-fi
+# Pull dev-resources jar from S3
+pushd dev-resources
+# Fallback to building the jar if the S3 pull failed
+echo -e "Building dev-resources..."
+mvn clean install
+popd
