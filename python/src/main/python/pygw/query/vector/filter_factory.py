f6f7456f3eb753f98a940d5b3457b125fd7388f4
Johnathan Garrett <jd@prominentedge.com>
2019-08-01 16:08:34 -0500

    Add pygw example Jupyter notebook to examples project
    Signed-off-by: Johnathan Garrett <jd@prominentedge.com>

--- python/src/main/python/pygw/query/vector/filter_factory.py
+++ python/src/main/python/pygw/query/vector/filter_factory.py
@@ -7,6 +7,8 @@
 # available at http://www.apache.org/licenses/LICENSE-2.0.txt
 #===============================================================================================
 
+from datetime import datetime
+
 from shapely.geometry.base import BaseGeometry
 
 from pygw.base import GeoWaveObject
@@ -113,6 +115,9 @@ class FilterFactory(GeoWaveObject):
         Returns:
             An Expression with the given literal value.
         """
+        if isinstance(value, datetime):
+            # Convert the date to a string
+            value = value.strftime("%Y-%m-%dT%H:%M:%S")
         if isinstance(value, str):
             # Prevent Py4J from assuming the string matches up with the char variant method
             filter_factory_class = self._java_ref.getClass()
@@ -392,8 +397,9 @@ class FilterFactory(GeoWaveObject):
         Args:
             expr (Expression): The expression to use.
             pattern (str): The pattern to match.
-            wildcard (str): The wildcard to use.  Default is None.
-            single_char (str): The single character to use.  Default is None.
+            wildcard (str): The string to use to match any characters.  Default is None.
+            single_char (str): The string to use to match a single character.  Default is None.
+            escape (str): The string to use to escape a wildcard.  Default is None.
             match_case (bool): Whether or not to match case with strings. Default is None.
             match_action (str): The match action to use.  Default is 'ANY'.
         Returns:
