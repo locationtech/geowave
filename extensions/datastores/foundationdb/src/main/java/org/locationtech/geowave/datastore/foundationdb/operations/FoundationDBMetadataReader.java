96db5420dc77c26109770608f4c29e1502149652
Kirill Chernyshov <kc875@cornell.edu>
2019-09-15 15:33:59 -0400

    Add skeleton code for fdb metadata reader class

---
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataReader.java
@@ -0,0 +1,23 @@
+package org.locationtech.geowave.datastore.foundationdb.operations;
+
+import org.locationtech.geowave.core.store.CloseableIterator;
+import org.locationtech.geowave.core.store.entities.GeoWaveMetadata;
+import org.locationtech.geowave.core.store.operations.MetadataQuery;
+import org.locationtech.geowave.core.store.operations.MetadataReader;
+import org.locationtech.geowave.core.store.operations.MetadataType;
+import org.locationtech.geowave.datastore.foundationdb.util.FoundationDBMetadataTable;
+
+public class FoundationDBMetadataReader implements MetadataReader {
+  private final FoundationDBMetadataTable table;
+  private final MetadataType metadataType;
+
+  public FoundationDBMetadataReader(final FoundationDBMetadataTable table, final MetadataType metadataType) {
+    this.table = table;
+    this.metadataType = metadataType;
+  }
+
+  @Override
+  public CloseableIterator<GeoWaveMetadata> query(MetadataQuery query) {
+    return null;
+  }
+}
