0b084fb8d5c649a660d96aa2b85c3e5a28ac9547
Xavier Lu <xll2@cornell.edu>
2019-09-15 15:13:09 -0400

    implement FDB metatable add

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FoundationDBMetadataTable.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FoundationDBMetadataTable.java
@@ -1,6 +1,8 @@
 package org.locationtech.geowave.datastore.foundationdb.util;
 
 import com.apple.foundationdb.Database;
+import com.google.common.primitives.Bytes;
+import com.google.common.primitives.Longs;
 import org.locationtech.geowave.core.store.entities.GeoWaveMetadata;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,15 +13,52 @@ import java.util.List;
 public class FoundationDBMetadataTable implements Closeable {
   private static final Logger LOGGER = LoggerFactory.getLogger(FoundationDBMetadataTable.class);
   private final Database db;
+  private final boolean requiresTimestamp;
+  private final boolean visibilityEnabled;
+  private long prevTime = Long.MAX_VALUE;
   private final List<FDBWrite> writes;
 
-  public FoundationDBMetadataTable(Database db) {
+  public FoundationDBMetadataTable(
+      Database db,
+      final boolean requiresTimestamp,
+      final boolean visibilityEnabled) {
     this.db = db;
+    this.requiresTimestamp = requiresTimestamp;
+    this.visibilityEnabled = visibilityEnabled;
     this.writes = new LinkedList<>();
   }
 
   public void add(final GeoWaveMetadata value) {
-    // TODO
+    byte[] key;
+    final byte[] secondaryId =
+        value.getSecondaryId() == null ? new byte[0] : value.getSecondaryId();
+    byte[] endBytes;
+    if (visibilityEnabled) {
+      final byte[] visibility = value.getVisibility() == null ? new byte[0] : value.getVisibility();
+
+      endBytes =
+          Bytes.concat(
+              visibility,
+              new byte[] {(byte) visibility.length, (byte) value.getPrimaryId().length});
+    } else {
+      endBytes = new byte[] {(byte) value.getPrimaryId().length};
+    }
+    if (requiresTimestamp) {
+      // sometimes rows can be written so quickly that they are the exact
+      // same millisecond - while Java does offer nanosecond precision,
+      // support is OS-dependent. Instead this check is done to ensure
+      // subsequent millis are written at least within this ingest
+      // process.
+      long time = Long.MAX_VALUE - System.currentTimeMillis();
+      if (time >= prevTime) { // this makes the timestamp unique
+        time = prevTime - 1;
+      }
+      prevTime = time;
+      key = Bytes.concat(value.getPrimaryId(), secondaryId, Longs.toByteArray(time), endBytes);
+    } else {
+      key = Bytes.concat(value.getPrimaryId(), secondaryId, endBytes);
+    }
+    write(key, value.getValue());
   }
 
   public void write(final byte[] key, final byte[] value) {
