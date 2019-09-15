f03fc47676ccca0d13bebdf2dbd99e73ae01e5bb
Kirill Chernyshov <kc875@cornell.edu>
2019-09-15 14:55:57 -0400

    Fix formatting

--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataWriter.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/operations/FoundationDBMetadataWriter.java
@@ -5,28 +5,28 @@ import org.locationtech.geowave.core.store.operations.MetadataWriter;
 import org.locationtech.geowave.datastore.foundationdb.util.FoundationDBMetadataTable;
 
 public class FoundationDBMetadataWriter implements MetadataWriter {
-    private final FoundationDBMetadataTable table;
-    private boolean closed = false;
+  private final FoundationDBMetadataTable table;
+  private boolean closed = false;
 
-    public FoundationDBMetadataWriter(FoundationDBMetadataTable table) {
-        this.table = table;
-    }
+  public FoundationDBMetadataWriter(FoundationDBMetadataTable table) {
+    this.table = table;
+  }
 
-    @Override
-    public void write(GeoWaveMetadata metadata) {
-        table.add(metadata);
-    }
+  @Override
+  public void write(GeoWaveMetadata metadata) {
+    table.add(metadata);
+  }
 
-    @Override
-    public void flush() {
-        table.flush();
-    }
+  @Override
+  public void flush() {
+    table.flush();
+  }
 
-    @Override
-    public void close() {
-        if (!closed) {
-            flush();
-            closed = true;
-        }
+  @Override
+  public void close() {
+    if (!closed) {
+      flush();
+      closed = true;
     }
+  }
 }
--- extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FDBWrite.java
+++ extensions/datastores/foundationdb/src/main/java/org/locationtech/geowave/datastore/foundationdb/util/FDBWrite.java
@@ -3,15 +3,15 @@ package org.locationtech.geowave.datastore.foundationdb.util;
 import com.apple.foundationdb.Transaction;
 
 public class FDBWrite {
-    private final byte[] key;
-    private final byte[] value;
+  private final byte[] key;
+  private final byte[] value;
 
-    public FDBWrite(byte[] key, byte[] value) {
-        this.key = key;
-        this.value = value;
-    }
+  public FDBWrite(byte[] key, byte[] value) {
+    this.key = key;
+    this.value = value;
+  }
 
-    public void add(Transaction txn) {
-        txn.set(key, value);
-    }
+  public void add(Transaction txn) {
+    txn.set(key, value);
+  }
 }
