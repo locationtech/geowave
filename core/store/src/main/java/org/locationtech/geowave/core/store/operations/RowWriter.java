d22accb3c8eeb4391f5d7b20ef66d151692654f6
Kirill Chernyshov <kc875@cornell.edu>
2019-12-10 21:02:43 -0500

    Squash core documentation changes into new branch
    Signed-off-by: Kirill Chernyshov <kc875@cornell.edu>

--- core/store/src/main/java/org/locationtech/geowave/core/store/operations/RowWriter.java
+++ core/store/src/main/java/org/locationtech/geowave/core/store/operations/RowWriter.java
@@ -16,9 +16,29 @@ import org.locationtech.geowave.core.store.entities.GeoWaveRow;
  * BatchWriter but can be overridden for other mechanisms to write the data.
  */
 public interface RowWriter extends AutoCloseable {
-  public void write(GeoWaveRow[] rows);
+  /**
+   * Write multiple GeoWave rows to the DB.
+   *
+   * Preconditions: <ul> <li>The writer is not closed</li> </ul>
+   *
+   * @param rows The array of rows to be written.
+   */
+  void write(GeoWaveRow[] rows);
 
-  public void write(GeoWaveRow row);
+  /**
+   * Write a GeoWave row to the DB.
+   *
+   * Preconditions: <ul> <li>The writer is not closed</li> </ul>
+   *
+   * @param row The row to be written.
+   */
+  void write(GeoWaveRow row);
 
-  public void flush();
+  /**
+   * Flush the writer, committing all pending writes. Note that the writes may already be committed
+   * - this method just establishes that they *must* be committed after the method returns.
+   *
+   * Preconditions: <ul> <li>The writer is not closed</li> </ul>
+   */
+  void flush();
 }
