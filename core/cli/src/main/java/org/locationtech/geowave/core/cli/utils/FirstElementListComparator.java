219edccb1012173e08bbe6b5b1281863478d3dd4
jhickman-prominent <50153272+jhickman-prominent@users.noreply.github.com>
2020-01-06 23:59:01 -0600

    Add ListStore and DescribeStore commands; genericize console output (#1657)
    Signed-off-by: jhickman <jhickman@prominentedge.com>

---
+++ core/cli/src/main/java/org/locationtech/geowave/core/cli/utils/FirstElementListComparator.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (c) 2013-2019 Contributors to the Eclipse Foundation
+ *
+ * <p> See the NOTICE file distributed with this work for additional information regarding copyright
+ * ownership. All rights reserved. This program and the accompanying materials are made available
+ * under the terms of the Apache License, Version 2.0 which accompanies this distribution and is
+ * available at http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package org.locationtech.geowave.core.cli.utils;
+
+import java.io.Serializable;
+import java.util.Comparator;
+import java.util.List;
+
+/**
+ * Performs a StringValue comparison of only the first element of equal-sized Lists of Objects, and
+ * trivial sorting rules for lists.
+ */
+public class FirstElementListComparator implements Comparator<List<Object>>, Serializable {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  public int compare(List<Object> listThis, List<Object> listOther) {
+    // Re-factored to this awkward structure because of Spot Bugs
+    if (listThis == null) {
+      if (listOther == null) {
+        return 0; // Consider both null as "equal"
+      } else {
+        return -1; // Null sorts ahead of non-null
+      }
+    } else if (listOther == null) {
+      return 1; // Null sorts ahead of non-null
+    }
+
+    // At this point, neither list can be null
+    if (listThis.size() != listOther.size()) {
+      return listThis.size() - listOther.size(); // shorter list ahead of longer list
+    } else { // lists are equal length
+      if (listThis.size() > 0) {
+        String strThis = String.valueOf(listThis.get(0) == null ? "" : listThis.get(0));
+        String strOther = String.valueOf(listOther.get(0) == null ? "" : listOther.get(0));
+        return strThis.compareTo(strOther);
+      } else { // both lists are length zero
+        return 0;
+      }
+    }
+  }
+
+}
---
+++ core/store/src/main/java/org/locationtech/geowave/core/store/cli/store/ListStoresCommand.java
@@ -0,0 +1,72 @@
+/**
+ * Copyright (c) 2013-2019 Contributors to the Eclipse Foundation
+ *
+ * <p> See the NOTICE file distributed with this work for additional information regarding copyright
+ * ownership. All rights reserved. This program and the accompanying materials are made available
+ * under the terms of the Apache License, Version 2.0 which accompanies this distribution and is
+ * available at http://www.apache.org/licenses/LICENSE-2.0.txt
+ */
+package org.locationtech.geowave.core.store.cli.store;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.stream.Collectors;
+import org.locationtech.geowave.core.cli.annotations.GeowaveOperation;
+import org.locationtech.geowave.core.cli.api.OperationParams;
+import org.locationtech.geowave.core.cli.api.ServiceEnabledCommand;
+import org.locationtech.geowave.core.cli.operations.config.options.ConfigOptions;
+import org.locationtech.geowave.core.cli.utils.ConsolePrinter;
+import org.locationtech.geowave.core.cli.utils.FirstElementListComparator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.beust.jcommander.Parameters;
+
+@GeowaveOperation(name = "list", parentOperation = StoreSection.class)
+@Parameters(commandDescription = "List non-default geowave data stores and their associated type")
+public class ListStoresCommand extends ServiceEnabledCommand<Map<String, String>> {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ListStoresCommand.class);
+
+
+  @Override
+  public void execute(OperationParams params) throws Exception {
+    Map<String, String> storeMap = computeResults(params);
+    List<List<Object>> rows = new ArrayList<List<Object>>(storeMap.size());
+    storeMap.entrySet().forEach(entry -> {
+      List<Object> values = new ArrayList<Object>(2);
+      String key = entry.getKey();
+      values.add(key.substring(6, key.length() - ".type".length()));
+      values.add(entry.getValue());
+      rows.add(values);
+    });
+
+    Collections.sort(rows, new FirstElementListComparator());
+    new ConsolePrinter().print(Arrays.asList("Data Store", "Type"), rows);
+  }
+
+  @Override
+  public Map<String, String> computeResults(OperationParams params) throws Exception {
+    final File configFile = getGeoWaveConfigFile(params);
+
+    // ConfigOptions checks/will never return null Properties
+    Properties configProps = ConfigOptions.loadProperties(configFile);
+    LOGGER.debug(configProps.size() + " entries in the config file");
+
+    // The name that the user gave the store is in a property named
+    // as "store." <[optional namespace.] the name the user gave > ".type"
+    Map<String, String> storeMap =
+        configProps.entrySet().stream().filter(
+            entry -> !entry.getKey().toString().startsWith("store.default-")) // Omit defaults
+            .filter(entry -> entry.getKey().toString().startsWith("store.")).filter(
+                entry -> entry.getKey().toString().endsWith(".type")).collect(
+                    Collectors.toMap(
+                        entry -> (String) entry.getKey(),
+                        entry -> (String) entry.getValue()));
+    return storeMap;
+  }
+
+}
