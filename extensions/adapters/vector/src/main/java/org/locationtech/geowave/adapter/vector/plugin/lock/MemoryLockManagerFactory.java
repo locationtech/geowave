73a4fa82b881462987255838af14dfa54c8bb774
Johnathan Garrett <jd@prominentedge.com>
2020-02-19 17:47:37 -0500

    Update copyright headers
    Signed-off-by: Johnathan Garrett <jd@prominentedge.com>

--- extensions/adapters/vector/src/main/java/org/locationtech/geowave/adapter/vector/plugin/lock/MemoryLockManagerFactory.java
+++ extensions/adapters/vector/src/main/java/org/locationtech/geowave/adapter/vector/plugin/lock/MemoryLockManagerFactory.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2013-2019 Contributors to the Eclipse Foundation
+ * Copyright (c) 2013-2020 Contributors to the Eclipse Foundation
  *
  * <p> See the NOTICE file distributed with this work for additional information regarding copyright
  * ownership. All rights reserved. This program and the accompanying materials are made available
