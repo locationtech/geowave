e174d16f8205f1246612db5228511d36cc227dad
Rich Fecher <rich.fecher@blacklynx.tech>
2020-05-04 12:40:22 -0400

    a couple minor conveniences for dynamodb
    Signed-off-by: Rich Fecher <rich.fecher@blacklynx.tech>

--- extensions/cli/dynamodb-embed/src/main/java/org/locationtech/geowave/datastore/dynamodb/cli/RunDynamoDBLocalOptions.java
+++ extensions/cli/dynamodb-embed/src/main/java/org/locationtech/geowave/datastore/dynamodb/cli/RunDynamoDBLocalOptions.java
@@ -8,25 +8,36 @@
  */
 package org.locationtech.geowave.datastore.dynamodb.cli;
 
-import java.io.File;
 import java.io.IOException;
 import com.beust.jcommander.Parameter;
 
 public class RunDynamoDBLocalOptions {
   @Parameter(names = {"--directory", "-d"}, description = "The directory to use for DynamoDB")
   private String directory = DynamoDBLocal.DEFAULT_DIR.getPath();
+  @Parameter(
+      names = {"--port", "-p"},
+      description = "The port to use for DynamoDB (defaults to " + DynamoDBLocal.DEFAULT_PORT + ")")
+  private Integer port = DynamoDBLocal.DEFAULT_PORT;
 
 
   public String getDirectory() {
     return directory;
   }
 
-  public void setDirectory(String directory) {
+  public void setDirectory(final String directory) {
     this.directory = directory;
   }
 
 
+  public Integer getPort() {
+    return port;
+  }
+
+  public void setPort(Integer port) {
+    this.port = port;
+  }
+
   public DynamoDBLocal getServer() throws IOException {
-    return new DynamoDBLocal(directory);
+    return new DynamoDBLocal(directory, port);
   }
 }
