5eb3cccd9b01c94c01fa8d66ba848daaf4ab5901
Rich Fecher <richard.fecher@maxar.com>
2021-05-29 08:27:29 -0400

    added back in retries and quiet build

--- .utility/run-tests.sh
+++ .utility/run-tests.sh
@@ -12,7 +12,7 @@ else
     wget -q https://archive.apache.org/dist/hadoop/common/hadoop-3.1.2/hadoop-3.1.2.tar.gz
     tar -xzf ./hadoop-3.1.2.tar.gz hadoop-3.1.2/lib/native/
     export LD_LIBRARY_PATH=$(pwd)/hadoop-3.1.2/lib/native/
-    mvn verify -am -pl test -Dtest=SkipUnitTests -Dfindbugs.skip -Dspotbugs.skip -DfailIfNoTests=false -P $MAVEN_PROFILES
+    mvn -q -B verify -am -pl test -Dtest=SkipUnitTests -Dfindbugs.skip -Dspotbugs.skip -DfailIfNoTests=false -P $MAVEN_PROFILES
   else
     echo -e "Running unit tests only w/ verify...\n"
     mvn -q -B verify -Dformatter.action=validate -P $MAVEN_PROFILES
