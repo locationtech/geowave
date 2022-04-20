2db47ce5bf704726b0dd4a2a43ea14402fb2c8c9
Milla Zagorski <Milla.Zagorski@maxar.com>
2022-04-20 11:57:07 -0400

    cruft removal

--- extensions/adapters/vector/src/main/java/org/locationtech/geowave/adapter/vector/plugin/GeoWaveGSProcessFactory.java
+++ extensions/adapters/vector/src/main/java/org/locationtech/geowave/adapter/vector/plugin/GeoWaveGSProcessFactory.java
@@ -24,7 +24,6 @@ public class GeoWaveGSProcessFactory extends AnnotatedBeanProcessFactory {
         "geowave",
         SubsampleProcess.class,
         DistributedRenderProcess.class,
-        MyPlugin.class,
         GeoWaveHeatMapFinal.class);
   }
 }
